2635d6b3a52f0ccaeb769146f1ee7c5005d61819
iraikov <none@none>
2015-04-29 13:24:04 -0700

    added connectivity export script

--- jobscripts/mpirun_Slice_50_300_Epileptic.sh
+++ jobscripts/mpirun_Slice_50_300_Epileptic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #$ -q som,asom,pub64
-#$ -pe mpi 256
+#$ -pe mpi 192
 #$ -cwd
 #$ -j y
 #$ -S /bin/bash
---
+++ scripts/export_connectivity_mpi.m
@@ -0,0 +1,52 @@
+
+output_dir=getenv('OUTPUT_DIR');
+syn_input_file=getenv('SYN_INPUT_FILE');
+gj_input_file=getenv('GJ_INPUT_FILE');
+batch_size=str2double(getenv('BATCH_SIZE'))
+batch_index=str2double(getenv('SGE_TASK_ID'))
+
+projection_names = {'GCtoGC';'MCtoGC';'HCtoGC';'BCtoGC';'AACtoGC';
+		    'HCCtoGC';'NGFCtoGC';'GCtoMC';'MCtoMC';'HCtoMC';
+		    'BCtoMC';'AACtoMC';'HCCtoMC';'GCtoHC';'MCtoHC';
+		    'HCtoHC';'BCtoHC';'GCtoBC';'MCtoBC';'HCtoBC';'BCtoBC';
+		    'HCCtoBC';'NGFCtoBC';'GCtoAAC';'MCtoAAC';'HCtoAAC';'NGFCtoAAC';
+		    'GCtoHCC';'MCtoHCC';'BCtoHCC';'HCCtoHCC';'NGFCtoHCC';'HCtoNGFC';'NGFCtoNGFC'}
+
+projections = [ 1 1; 2 1; 3 1; 4 1; 5 1; 6 1; 7 1; % GC
+		1 2; 2 2; 3 2; 4 2; 5 2; 6 2; 1 3; % MC
+	        2 3; 3 3; 4 3; % HC
+		1 4; 2 4; 3 4; 4 4; 6 4; 7 4; % BC
+	        1 5; 2 5; 3 5; 7 5; % GCC
+		1 6; 2 6; 4 6; 6 6; 7 6;  % AAC
+		3 7; 7 7 ]; % NGFC
+
+
+m = load(syn_input_file);
+
+for i = 1:size(projections,1)
+
+    name = projection_names{i}
+    r = projections(i,1); c = projections(i,2);
+    connmatrix_export(name,m.connection_M{r,c},batch_index,batch_size);
+    
+end
+
+gjprojection_names = {'HCtoHC';'BCtoBC';'HCCtoHCC';'NGFCtoNGFC'};
+gjprojections = [ 3 3; 4 4; 6 6; 7 7; ];
+
+gjms = load(gj_input_file);
+data = [];
+for i = 1:numel(gjprojections)
+        
+        name = gjprojections{i}
+        r = gjprojections(i,1); c = gjprojections(i,2);
+        
+        filename = sprintf('gj%s.dat', name);
+        fid  = fopen (filename,'w+');
+        fprintf(fid, '%d %d\n', size(data,1), size(data,2));
+        fclose(fid);
+        data = gjms.gap_junctions{r,c};
+        save('-ascii', '-append', filename, 'data');
+        
+end
+
