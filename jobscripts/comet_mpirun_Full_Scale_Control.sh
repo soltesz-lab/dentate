feff0fe0e4f1eb9f49afc44854b19d7e4f76795d
iraikov <none@none>
2015-05-14 19:55:21 -0700

    added comet running script

---
+++ jobscripts/comet_mpirun_Full_Scale_Control.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH -J dentate_Full_Scale_Control
+#SBATCH -o ./results/dentate_Full_Scale_Control.%j.o
+#SBATCH -n 256
+#SBATCH -p compute
+#SBATCH -t 4:00:00
+#SBATCH --mail-user=ivan.g.raikov@gmail.com
+#SBATCH --mail-type=END
+#SBATCH --mail-type=BEGIN
+#
+
+set -x
+
+mkdir -p ./results/Full_Scale_Control_$SLURM_JOB_ID
+
+ibrun ./mechanisms/x86_64/special -mpi -nobanner -nogui \
+-c "strdef parameters" -c "parameters=\"./parameters/Full_Scale_Control.hoc\"" \
+-c "strdef resultsPath" -c "resultsPath=\"./results/Full_Scale_Control_$SLURM_JOB_ID\"" \
+main.hoc
