7e81317b2878a0018b5b17359fae653c07a8bd22
Ivan Raikov <ivan.g.raikov@gmail.com>
2015-06-23 11:01:12 -0700

    added sherlock scripts for slice control experiment
    --HG--
    branch : stanford-sherlock

---
+++ jobscripts/sherlock_Slice_50_300_Control.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#$ -q som,asom,pub64
+#$ -pe mpi 128
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -N dentate_Slice_50_300_Control
+#$ -o ./results/dentate_Slice_50_300_Control.$JOB_ID.o
+#$ -R y
+
+module load neuron/7.4alpha
+
+mkdir -p ./results/Slice_50_300_Control_$JOB_ID
+
+mpirun -np $CORES nrniv -mpi -nobanner -nogui \
+-c "strdef parameters" -c "parameters=\"./parameters/sherlock_Slice_50_300_Control.hoc\"" \
+-c "strdef resultsPath" -c "resultsPath=\"./results/Slice_50_300_Control_$JOB_ID\"" \
+main.hoc
