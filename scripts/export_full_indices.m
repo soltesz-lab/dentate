03c3d753a6a20c0d580f5244b16247748e460db3
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-01-19 15:49:04 -0800

    added layer selection to PP projection script; exporting coordinates as well as indices

--- scripts/export_full_indices.m
+++ scripts/export_full_indices.m
@@ -7,18 +7,19 @@ populations = {'GC';'MC';'HC';'BC';'AAC';'HCC';'NGFC'}
 
 ax = 1;
 for i = 1:numel(populations)
-
+    
     name = populations{i}
 
     filename = sprintf('%s.dat', name);
     fid = fopen (filename,'w+');
     data = loc.locations{i,1};
     datasize = size(data,1);
-    fprintf(fid, '%d %d\n', datasize, 1);
+    fprintf(fid, '%d %d\n', datasize, 4);
     fclose(fid);
     inds = reshape(ax:(ax+datasize-1),[datasize,1]);
-    size(inds)
-    save('-ascii', '-append', filename, 'inds');
+    newdata = ([inds data(:,1:3)]);
+    size(newdata)
+    save('-ascii', '-append', filename, 'newdata');
     ax = ax+datasize;
 end
 
