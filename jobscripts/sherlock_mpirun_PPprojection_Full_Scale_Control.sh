7557029c4f88ef10b6c269454d68375eb4a65b45
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-02-18 10:53:00 -0800

    set PPprojection max radius to 6 um
    --HG--
    branch : stanford-sherlock

--- jobscripts/sherlock_mpirun_PPprojection_Full_Scale_Control.sh
+++ jobscripts/sherlock_mpirun_PPprojection_Full_Scale_Control.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH -J PPprojection_Full_Scale_Control
 #SBATCH -o ./results/PPprojection_Full_Scale_Control.%j.o
-#SBATCH --nodes=4
+#SBATCH --nodes=5
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem=8192
 #SBATCH -t 4:00:00
@@ -26,5 +26,5 @@ results_dir=/scratch/users/$USER/PPprojection_Full_Scale_Control_forest_${forest
 mkdir -p $results_dir
 cd $results_dir
 
-mpirun $HOME/model/dentate/scripts/DGnetwork/PPprojection -t $forest_dir -p $gridcell_dir -r 3.0 -o $results_dir \
+mpirun $HOME/model/dentate/scripts/DGnetwork/PPprojection -t $forest_dir -p $gridcell_dir -r 6.0 -o $results_dir \
  --grid-cells=10:3800
