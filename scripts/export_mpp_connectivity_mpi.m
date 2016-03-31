612338f88951f43173e9171a06a2355264a96e08
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-03-31 11:37:02 -0700

    added various support scripts for PP connectivity

---
+++ scripts/export_mpp_connectivity_mpi.m
@@ -0,0 +1,16 @@
+batch_size=256
+%batch_size=str2double(getenv('BATCH_SIZE'))
+
+for forest = 1:1000
+
+    mpp_input_file=sprintf('MPPtoDGC.%d.dat',forest)
+
+    tic;
+    m = dlmread(mpp_input_file, SEP=' ', R0=1, C0=0);
+    toc;
+
+    for batch_index = 1:batch_size
+        append_connmatrix('MPPtoDGC',double(m),1000000,batch_index,batch_size,sprintf('B%d',batch_size));
+    end
+
+end
