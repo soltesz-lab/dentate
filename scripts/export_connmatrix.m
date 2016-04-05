1c9ee8234ef92da2609ccab0b7e81a76e0ecb526
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-04-05 14:30:42 -0700

    using matfile in connectivity export script to reduce memory usage
    --HG--
    branch : stanford-sherlock

--- scripts/export_connmatrix.m
+++ scripts/export_connmatrix.m
@@ -1,14 +1,23 @@
-function export_connmatrix(name,m,offset,batch_index,batch_size,output_dir)
+function export_connmatrix(name,m,msize,popsize,offset,batch_index,batch_size,output_dir)
 
-    range = offset+(batch_index:batch_size:size(m,1));
-    data = m(ismembc(m(:,2),range),:);
-fprintf('export_connmatrix: name = %s offset = %d size(m) = %d size(data) = %d batch_index = %d\n', name, offset, size(m,1), size(data,1), batch_index)
+  range = offset+(batch_index:batch_size:popsize);
+  chunkSize=10000000;
+  chunkStart=1; chunkEnd = chunkSize+chunkStart;
+  while (chunkStart < msize)
+    if chunkEnd < msize
+       data = double(m(ismember(m(chunkStart:chunkEnd,2),range),:));
+    else
+       data = double(m(ismember(m(chunkStart:msize,2),range),:));
+    end
+    fprintf('export_connmatrix: name = %s offset = %d popsize = %d size(data) = %d batch_index = %d\n', name, offset, popsize, size(data,1), batch_index)
     filename = sprintf('%s/%s.%d.dat', output_dir, name, batch_index);
     if (not (isempty(data)))
-      fid = fopen (filename,'w+');
-      fprintf(fid, '%d %d\n', size(data,1), size(data,2));
-      fclose(fid);
+      %%fid = fopen (filename,'w+');
+      %%fprintf(fid, '%d %d\n', size(data,1), size(data,2));
+      %%fclose(fid);
       save('-ascii', '-append', filename, 'data');
     end
-
+    chunkStart = chunkEnd+1;
+    chunkEnd = chunkSize+chunkStart;
+  end
 end
