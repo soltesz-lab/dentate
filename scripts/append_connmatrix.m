ab93e6c3ece70e1321d50241cc1301dcafa7ebd0
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-04-25 17:04:21 -0700

    added scripts for exporting PP-BC connectivity

--- scripts/append_connmatrix.m
+++ scripts/append_connmatrix.m
@@ -1,7 +1,7 @@
 
-function append_connmatrix(name,m,popsize,batch_index,batch_size,output_dir)
+function append_connmatrix(name,m,popsize,offset,batch_index,batch_size,output_dir)
 
-    range = batch_index:batch_size:popsize;
+    range = offset + batch_index:batch_size:popsize;
     data = m(ismember(m(:,2),range),:);
     fprintf('append_connmatrix: name = %s  size(m) = %d size(data) = %d batch_index = %d\n', name, size(m,1), size(data,1), batch_index)
     filename = sprintf('%s/%s.%d.noheader.dat', output_dir, name, batch_index);
--- scripts/export_mppbc_connectivity_mpi.m
+++ scripts/export_mppbc_connectivity_mpi.m
@@ -1,16 +1,16 @@
 batch_size=512
 %batch_size=str2double(getenv('BATCH_SIZE'))
 
-for forest = 1:1000
+for i = 1:batch_size
 
-    mpp_input_file=sprintf('MPPtoDGC.%d.dat',forest)
+    mpp_input_file=sprintf('MPPtoBC.%d.dat',i)
 
     tic;
     m = dlmread(mpp_input_file, SEP=' ', R0=1, C0=0);
     toc;
 
     for batch_index = 1:batch_size
-        append_connmatrix('MPPtoDGC',double(m),1000000,0,batch_index,batch_size,sprintf('B%d',batch_size));
+        append_connmatrix('MPPtoBC',double(m),3800,1039001,batch_index,batch_size,sprintf('B%d',batch_size));
     end
 
 end
