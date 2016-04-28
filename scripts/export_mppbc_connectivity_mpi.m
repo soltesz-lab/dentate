83eb9364d912e4d03c0e001f94ca7084d7fedd1e
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-04-28 14:00:29 -0700

    some corrections to scripts that export PP-BC connectivity for a particular processor configuration

--- scripts/export_mppbc_connectivity_mpi.m
+++ scripts/export_mppbc_connectivity_mpi.m
@@ -1,17 +1,17 @@
 batch_size=512
 %batch_size=str2double(getenv('BATCH_SIZE'))
-srcbatch_size = 48
+srcbatch_size = 64
 
 for i = 1:srcbatch_size
 
-    mpp_input_file=sprintf('MPPtoBC.%d.dat',i)
+    mpp_input_file=sprintf('MPPtoBC.%d.dat',i-1)
 
     tic;
-    m = dlmread(mpp_input_file, SEP=' ', R0=1, C0=0);
+    m = dlmread(mpp_input_file, SEP=' ', R0=0, C0=0);
     toc;
 
     for batch_index = 1:batch_size
-        append_connmatrix('MPPtoBC',double(m),3800,1039001,batch_index,batch_size,sprintf('B%d',batch_size));
+        append_connmatrix('MPPtoBC',double(m),3800,1039000,batch_index,batch_size,sprintf('B%d',batch_size));
     end
 
 end
