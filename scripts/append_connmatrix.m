ff27cdab175f67480f7fadbba5c67ec556313315
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-06-09 15:49:27 -0700

    corrections to cell indexing in mpi connectivity exports scripts

--- scripts/append_connmatrix.m
+++ scripts/append_connmatrix.m
@@ -2,7 +2,8 @@
 function append_connmatrix(name,m,popsize,offset,batch_index,batch_size,output_dir)
 
     range = offset+(batch_index:batch_size:popsize);
-    data = m(ismember(m(:,2),range),:);
+    res = find(ismember(m(:,2),range));
+    data = m(res,:);
     fprintf('append_connmatrix: name = %s  size(m) = %d size(data) = %d batch_index = %d\n', name, size(m,1), size(data,1), batch_index)
     filename = sprintf('%s/%s.%d.noheader.dat', output_dir, name, batch_index);
     if (not (isempty(data)))
