62f3b6fea0ca339b9e3d34f89fcb23db5ed45633
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-06-23 14:00:53 -0700

    some renaming of variables in npcloud; increasing duration of trajectory and grid cell activity

--- scripts/GridCells/gen_ratemap.m
+++ scripts/GridCells/gen_ratemap.m
@@ -27,7 +27,7 @@ savegrid  = 1;
 ratemap  = grid_ratemap(X,Y,lambda,theta,xoff,yoff);
 
 dt = 0.01; % ms
-tend = 150; % ms
+tend = 650; % ms
 [Xpos,Ypos] = random_walk(tend, dt);
 
 size_len = sqrt(grid_unit);
@@ -35,10 +35,11 @@ npts  = round(W/size_len);
 Xgrid = round(Xpos/npts);
 Ygrid = round(Ypos/npts);
 
-T = size(Xgrid,1);
+T = size(Xgrid,1)
 rbar  = zeros(T,N);
 
 for t = 1:T
+    t
     rbar(t,:) = ratemap(:,Xgrid(t),Ygrid(t));
 end
 size(rbar)
@@ -51,8 +52,7 @@ grid_data.Xpos = Xpos;
 grid_data.Ypos = Ypos;
 grid_data.Xgrid = Xgrid;
 grid_data.Ygrid = Ygrid;
-grid_data.rbar = rbar;
 
-save("grid_data.mat","grid_data");
+save('grid_data.mat','grid_data','rbar');
 
 
