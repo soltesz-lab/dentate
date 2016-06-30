6eccb36704ac736052665a561b9bc98c552244f8
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-06-30 12:25:13 -0500

    added Blue Waters parameters and runfile

---
+++ jobscripts/bluewaters_mpirun_Full_Scale_Control.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### set the number of nodes and the number of PEs per node
+#PBS -l nodes=256:ppn=2:xe
+### set the wallclock time
+#PBS -l walltime=4:00:00
+### set the job name
+#PBS -N dentate_Full_Scale_Control
+### set the job stdout and stderr
+#PBS -e $PBS_JOBID.err
+#PBS -o $PBS_JOBID.out
+### set email notification
+##PBS -m bea
+### Set umask so users in my group can read job stdout and stderr files
+#PBS -W umask=0027
+### save Darshan profile data
+#PBS -lgres=darshan
+
+
+module unload PrgEnv-cray
+module load PrgEnv-intel
+
+set -x
+
+cd $HOME/model/dentate
+
+mkdir -p ./results/Full_Scale_Control_$PBS_JOBID
+
+aprun -n 512 ./mechanisms/x86_64/special -mpi -nobanner -nogui -c "strdef parameters" -c "parameters=\"./parameters/bluewaters_Full_Scale_Control.hoc\"" -c "strdef resultsPath" -c "resultsPath=\"./results/Full_Scale_Control_$PBS_JOBID\"" main.hoc
+
+mv ${PBS_JOBID}.err ${PBS_JOBID}.out ./results/Full_Scale_Control_$PBS_JOBID
