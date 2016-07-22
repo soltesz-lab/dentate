73680018778cc4ab292a35eef4c446bb96d3e8fd
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-07-22 16:25:26 -0700

    sample a subset of the input spike trains in spike distance script

--- scripts/spike-distance.py
+++ scripts/spike-distance.py
@@ -1,4 +1,5 @@
 import sys
+import random
 import numpy as np
 import pyspike as spk
 import matplotlib
@@ -9,9 +10,12 @@ import matplotlib.pyplot as plt
 inputfile = sys.argv[1]
 tstop = 500
 
-spike_trains = spk.load_spike_trains_from_txt(inputfile, edges=(0, tstop), is_sorted=False)
+spike_trains = spk.load_spike_trains_from_txt(inputfile, edges=(0, tstop))
 
-isi_profile = spk.isi_profile(spike_trains)
+spike_trains_sample = random.sample(spike_trains, 2500)
+
+#isi_profile = spk.isi_profile(spike_trains)
+isi_profile = spk.isi_profile(spike_trains_sample)
 x, y = isi_profile.get_plottable_data()
 
 fig = plt.figure()
@@ -24,7 +28,8 @@ print("Avg ISI distance: %.8f" % isi_profile.avrg())
 
 fig = plt.figure()
 
-spike_profile = spk.spike_profile(spike_trains)
+#spike_profile = spk.spike_profile(spike_trains)
+spike_profile = spk.spike_profile(spike_trains_sample)
 x, y = spike_profile.get_plottable_data()
 plt.plot(x, y, '--k')
 
