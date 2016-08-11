4d380745e01f9b1aa45e868e2004f1060d288c49
Ivan Raikov <ivan.g.raikov@gmail.com>
2016-08-11 12:42:46 -0500

    increased run time limit in bluewaters script

--- jobscripts/bluewaters_mpirun_Full_Scale_Control.sh
+++ jobscripts/bluewaters_mpirun_Full_Scale_Control.sh
@@ -3,12 +3,12 @@
 ### set the number of nodes and the number of PEs per node
 #PBS -l nodes=64:ppn=8:xe
 ### set the wallclock time
-#PBS -l walltime=18:00:00
+#PBS -l walltime=24:00:00
 ### set the job name
 #PBS -N dentate_Full_Scale_Control
 ### set the job stdout and stderr
-#PBS -e $PBS_JOBID.err
-#PBS -o $PBS_JOBID.out
+#PBS -e ./results/$PBS_JOBID.err
+#PBS -o ./results/$PBS_JOBID.out
 ### set email notification
 ##PBS -m bea
 ### Set umask so users in my group can read job stdout and stderr files
@@ -16,7 +16,7 @@
 ### Get darsan profile data
 #PBS -lgres=darshan
 
-module swap PrgEnv-cray PrgEnv-intel
+module swap PrgEnv-cray PrgEnv-gnu
 
 set -x
 
@@ -34,6 +34,9 @@ runhoc="./jobscripts/bluewaters_Full_Scale_Control_run_${PBS_JOBID}.hoc"
 
 sed -e "s/JOB_ID/$PBS_JOBID/g" ./jobscripts/bluewaters_Full_Scale_Control_run.hoc > $runhoc
 
-LD_PRELOAD=/sw/xe/darshan/2.3.0/darshan-2.3.0_cle52/lib/libdarshan.so aprun -n 512 ./mechanisms/x86_64/special -mpi -nobanner -nogui $runhoc
+##export LD_PRELOAD=/sw/xe/darshan/2.3.0/darshan-2.3.0_cle52/lib/libdarshan.so 
+export DARSHAN_LOGPATH=$PWD/darshan-logs
 
-mv ${PBS_JOBID}.err ${PBS_JOBID}.out $results_path
+aprun -n 512 ./mechanisms/x86_64/special -mpi -nobanner -nogui $runhoc
+
+
