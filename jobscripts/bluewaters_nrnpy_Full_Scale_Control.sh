eed470362c0da53ee78a81f4889532aa42957cf1
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-01-12 13:06:57 -0600

    removing some old hoc scripts; committing changes to base control job scripts

--- jobscripts/bluewaters_nrnpy_Full_Scale_Control.sh
+++ jobscripts/bluewaters_nrnpy_Full_Scale_Control.sh
@@ -3,11 +3,11 @@
 ### set the number of nodes and the number of PEs per node
 #PBS -l nodes=2048:ppn=1:xe
 ### which queue to use
-#PBS -q debug
+#PBS -q high
 ### set the wallclock time
-#PBS -l walltime=00:30:00
+#PBS -l walltime=3:00:00
 ### set the job name
-#PBS -N dentate_debug_Full_Scale_Control
+#PBS -N dentate_Full_Scale_Control
 ### set the job stdout and stderr
 #PBS -e ./results/$PBS_JOBID.err
 #PBS -o ./results/$PBS_JOBID.out
@@ -15,33 +15,43 @@
 ##PBS -m bea
 ### Set umask so users in my group can read job stdout and stderr files
 #PBS -W umask=0027
-### Get darsan profile data
-#PBS -lgres=darshan
 
 module swap PrgEnv-cray PrgEnv-gnu
 module unload gcc
 module load gcc/5.1.0
-module load bwpy
-module load bwpy-mpi
+module load bwpy bwpy-mpi
+module load cray-hdf5-parallel
 
 set -x
 
-cd $PBS_O_WORKDIR
+export PYTHONPATH=/projects/sciteam/baef/nrn/lib/python:$PYTHONPATH
+export LD_LIBRARY_PATH=/sw/bw/bwpy/0.3.0/python-mpi/usr/lib:/sw/bw/bwpy/0.3.0/python-single/usr/lib:$LD_LIBRARY_PATH
+export PATH=/projects/sciteam/baef/nrn/x86_64/bin:$PATH
+export DARSHAN_LOGPATH=$PWD/darshan-logs
+
+echo python is `which python`
 
 results_path=./results/Full_Scale_Control_$PBS_JOBID
 export results_path
+
+cd $PBS_O_WORKDIR
 
 mkdir -p $results_path
 
 git ls-files | tar -zcf ${results_path}/dentate.tgz --files-from=/dev/stdin
 git --git-dir=../dgc/.git ls-files | grep Mateos-Aparicio2014 | tar -C ../dgc -zcf ${results_path}/dgc.tgz --files-from=/dev/stdin
 
-runhoc="./jobscripts/bluewaters_Full_Scale_Control_debug_run_${PBS_JOBID}.hoc"
+aprun -n 2048 nrniv -mpi -python main.py \
+    --model-name=dentatenet \
+    --dataset-name=Full_Scale_Control \
+    --dataset-prefix="/u/sciteam/raikov/model/dentate/datasets/B2048" \
+    --results-path=$results_path \
+    --io-size=128 \
+    --tstop=1000 \
+    --v-init=-75 \
+    --max-walltime-hours=3
 
-sed -e "s/JOB_ID/$PBS_JOBID/g" ./jobscripts/bluewaters_Full_Scale_Control_debug_run.hoc > $runhoc
 
-export DARSHAN_LOGPATH=$PWD/darshan-logs
 
-aprun -n 2048 ./mechanisms/x86_64/special -mpi -nobanner -nogui $runhoc
 
 
