09c0004264cdf1282fe4317416d446bc39a7c5e0
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-03-22 14:25:28 -0700

    adding mec/lec activity generation scripts

--- jobscripts/sherlock_ca3_place_cell_spikes.sh
+++ jobscripts/sherlock_ca3_place_cell_spikes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH -J place_cell_spikes
-#SBATCH -o ./results/place_cell_spikes.%j.o
+#SBATCH -o ./results/ca3_place_cell_spikes.%j.o
 #SBATCH -t 12:00:00
 #SBATCH --mem 8192
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
@@ -11,11 +11,10 @@
 
 module load matlab
 
-BATCH_SIZE=804
+BATCH_SIZE=400
 BATCH_INDEX=$SLURM_ARRAY_TASK_ID
-OUTPUT_PATH=$SCRATCH/lec3_placecells
-INPUT_DATA_PATH=$SCRATCH/lec3_placecells/linear_lec3_place_data.mat
-OUTPUT_PATH=$SCRATCH/lec3_placecells
+OUTPUT_PATH=$SCRATCH/ca3_placecells
+INPUT_DATA_PATH=$SCRATCH/ca3_placecells/linear_ca3_placecell_data.bin
 
 export BATCH_SIZE
 export BATCH_INDEX
@@ -23,6 +22,6 @@ export OUTPUT_PATH
 export INPUT_DATA_PATH
 
 cd $HOME/model/dentate/scripts/SpatialCells
-./run_gen_lec3_placecell_spikes.sh /share/sw/licensed/MATLAB-R2016b
+./run_gen_ca3_placecell_spikes.sh /share/sw/licensed/MATLAB-R2016b
 
 
---
+++ scripts/SpatialCells/gen_lec3_placecell_spikes.m
@@ -0,0 +1,17 @@
+
+
+batch_size=str2num(getenv('BATCH_SIZE'))
+batch_index=str2num(getenv('BATCH_INDEX'))
+input_data_path=getenv('INPUT_DATA_PATH')
+output_path=getenv('OUTPUT_PATH');						     
+
+linear_place_data=matfile(input_data_path);
+
+rbar = permute(linear_place_data.place_rbar_modules((((batch_index-1)*batch_size)+1):(batch_index*batch_size),:),[2,1]) * 5e-3;
+
+[T,N] = size(rbar)
+  
+s = DG_SFromPSTHVarZ(rbar, 1);
+spikes = DG_spike_gen(s,eye(N,N),1);
+
+save(sprintf('%s/lec3_place_spikes_%d.mat',output_path,batch_index),'spikes');
