ff09e0d2ed198676b5de3aea24a56eeba0860acd
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-04-05 10:16:39 -0700

    correct N_LPP numbers

--- scripts/DGnetwork/DGnetwork_12_s_somapos.m
+++ scripts/DGnetwork/DGnetwork_12_s_somapos.m
@@ -18,9 +18,9 @@ MML_layer_min       = 1;
 MML_layer_mid       = 1.5;
 MML_layer_max       = 2;
 
-MOML_layer_min       = 1;
-MOML_layer_mid       = 2;
-MOML_layer_max       = 3.1;
+OML_layer_min       = 2;
+OML_layer_mid       = 2.5;
+OML_layer_max       = 3.1;
 
 % indices for each cell type
 N_MC       = 2;
@@ -43,74 +43,81 @@ distribution    = cell(num_types,1);
 soma_points     = cell(num_types,1);
 
 %2 Mossy
-num_cells{N_MC}    = [30000;0;0;0;30000]; %Hilus;GCL;IML;MOML;Total
+num_cells{N_MC}    = [30000;0;0;0;0;30000]; %Hilus;GCL;IML;MML;OML;Total
 soma_disth(N_MC)   = 20;
 soma_distv(N_MC)   = 20;
 septotemporal      = [20.8;38.2;67.6;75.4;51.2;48.6;84.1;68.5;145.7;385.0];
 distribution{N_MC} = septotemporal/sum(septotemporal(:,1));
 
 %3 HIPP
-num_cells{N_HIPP}    = [9000;0;0;0;9000];
+num_cells{N_HIPP}    = [9000;0;0;0;0;9000];
 soma_disth(N_HIPP)  = 50;
 soma_distv(N_HIPP)  = 50;
 septotemporal   = [16.2;35.3;38.1;31.8;36.1;38.8;39.6;55.0;73.1;141.5];
 distribution{N_HIPP} = septotemporal/sum(septotemporal(:,1));
 
 %4 PVBC
-num_cells{N_PVBC}    = [1700;1700;400;0;3800];
+num_cells{N_PVBC}    = [1700;1700;400;0;0;3800];
 soma_disth(N_PVBC)   = 80;
 soma_distv(N_PVBC)   = 80;
 septotemporal   = [9.5;16.5;19.6;17.5;16.8;15.7;15.8;17.5;21.2;40.9];
 distribution{N_PVBC} = septotemporal/sum(septotemporal(:,1));
 
 %5 AA 
-num_cells{N_AA}    = [200;200;50;0;450];
+num_cells{N_AA}    = [200;200;50;0;0;450];
 soma_disth(N_AA)   = 100;
 soma_distv(N_AA)   = 100;
 septotemporal   = [9.5;16.5;19.6;17.5;16.8;15.7;15.8;17.5;21.2;40.9];
 distribution{N_AA} = septotemporal/sum(septotemporal(:,1));
 
 %6 HICAP/CCK+ Basket Cells
-num_cells{N_HICAP}    = [1150;250;0;0;1400];
+num_cells{N_HICAP}    = [1150;250;0;0;0;1400];
 soma_disth(N_HICAP)   = 100;
 soma_distv(N_HICAP)   = 100;
 septotemporal   = [9.1;16.3;13.1;11.3;9.3;8.3;8.3;9.9;9.8;6.1];
 distribution{N_HICAP} = septotemporal/sum(septotemporal(:,1));
 
 %7 NGFC
-num_cells{N_NGFC}    = [0;0;0;5000;5000];
+num_cells{N_NGFC}    = [0;0;0;2500;2500;5000];
 soma_disth(N_NGFC)   = 50;
 soma_distv(N_NGFC)   = 50;
 septotemporal   = [9.5;16.5;19.6;17.5;16.8;15.7;15.8;17.5;21.2;40.9];
 distribution{N_NGFC} = septotemporal/sum(septotemporal(:,1));
 
 %8 IS
-num_cells{N_IS}    = [3000;0;0;0;3000];
+num_cells{N_IS}    = [3000;0;0;0;0;3000];
 soma_disth(N_IS)   = 50;
 soma_distv(N_IS)   = 50;
 septotemporal   = [16.2;35.3;38.1;31.8;36.1;38.8;39.6;55.0;73.1;141.5];
 distribution{N_IS} = septotemporal/sum(septotemporal(:,1));
 
 %9 MOPP
-num_cells{N_MOPP}    = [0;0;2000;1000;3000];
+num_cells{N_MOPP}    = [0;0;2000;500;500;3000];
 soma_disth(N_MOPP)   = 80;
 soma_distv(N_MOPP)   = 80;
 septotemporal   = [16.2;35.3;38.1;31.8;36.1;38.8;39.6;55.0;73.1;141.5];
 distribution{N_MOPP} = septotemporal/sum(septotemporal(:,1));
 
 %10 MPP
-num_cells{N_MPP}    = [0;0;0;38000;38000];
+num_cells{N_MPP}    = [0;0;0;38000;0;38000];
 soma_disth(N_MPP)   = 20;
 soma_distv(N_MPP)   = 20;
 septotemporal   = [16.2;35.3;38.1;31.8;36.1;38.8;39.6;55.0;73.1;141.5];
 distribution{N_MPP} = septotemporal/sum(septotemporal(:,1));
+
+%11 LPP
+num_cells{N_LPP}    = [0;0;0;0;34000;34000];
+soma_disth(N_LPP)   = 20;
+soma_distv(N_LPP)   = 20;
+septotemporal   = [16.2;35.3;38.1;31.8;36.1;38.8;39.6;55.0;73.1;141.5];
+distribution{N_LPP} = septotemporal/sum(septotemporal(:,1));
 
 
 
 for i = 2:num_types
     i
     soma_points = [];
-    for section = 1:4
+    for section = 1:5
         section
         switch section
         case 1
@@ -137,7 +144,14 @@ for i = 2:num_types
         case 4
           k = num_cells{i}(section);
           if k > 0
-             soma_grid = DGnetwork_11_s_somagrid(soma_disth(i),soma_distv(i),@layer_eq_ML,MOML_layer_min,MOML_layer_mid,MOML_layer_max);
+             soma_grid = DGnetwork_11_s_somagrid(soma_disth(i),soma_distv(i),@layer_eq_ML,MML_layer_min,MML_layer_mid,MML_layer_max);
+             sz = size(soma_grid);
+             soma_points = vertcat(soma_points,soma_grid(randsample(sz(1),k),:));
+          end
+        case 5
+          k = num_cells{i}(section);
+          if k > 0
+             soma_grid = DGnetwork_11_s_somagrid(soma_disth(i),soma_distv(i),@layer_eq_ML,OML_layer_min,OML_layer_mid,OML_layer_max);
              sz = size(soma_grid);
              soma_points = vertcat(soma_points,soma_grid(randsample(sz(1),k),:));
           end
