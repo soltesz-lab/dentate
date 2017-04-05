941eb683c675480c093af1aa0df617b58f1eec6b
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-04-05 13:41:20 -0700

    added somapos job scripts

---
+++ jobscripts/sherlock_DGnetwork_somapos.m
@@ -0,0 +1,4 @@
+addpath /home/iraikov/model/dentate/scripts/DGnetwork
+addpath /home/iraikov/model/dgc_forest/lib
+
+DGnetwork_12_s_somapos();
---
+++ jobscripts/sherlock_DGnetwork_somapos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH -J DGnetwork_somapos
+#SBATCH -o ./results/DGnetwork_somapos.%j.o
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=10240
+#SBATCH -t 2:00:00
+#SBATCH --mail-user=ivan.g.raikov@gmail.com
+#SBATCH --mail-type=END
+#
+
+set -x
+
+module load matlab
+
+matlab -nojvm -r ./jobscripts/sherlock_DGnetwork_somapos.m
