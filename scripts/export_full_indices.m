a64ab53b9417286200cfddca084cf9570094e302
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-04-05 15:39:42 -0700

    updated export indices script with MOPP and IS cells

--- scripts/export_full_indices.m
+++ scripts/export_full_indices.m
@@ -2,22 +2,22 @@
 args = argv ();
 
 loc = load(args{1});
+populations = {'GC';'MC';'HC';'BC';'AAC';'HCC';'NGFC';'IS';'MOPP';'MPP';'LPP'}
 
-populations = {'GC';'MC';'HC';'BC';'AAC';'HCC';'NGFC'}
-
-ax = 1;
+ax = 0;
 for i = 1:numel(populations)
     
     name = populations{i}
 
     filename = sprintf('%s.dat', name);
-    fid = fopen (filename,'w+');
-    data = loc.locations{i,1};
+    fid      = fopen (filename,'w+');
+    data     = loc.soma_locations{i,1};
     datasize = size(data,1);
-    fprintf(fid, '%d %d\n', datasize, 4);
+    ncol     = size(data,2);
+    fprintf(fid, '%d %d\n', datasize, ncol+1);
     fclose(fid);
     inds = reshape(ax:(ax+datasize-1),[datasize,1]);
-    newdata = ([inds data(:,1:3)]);
+    newdata = ([inds data(:,1:ncol)]);
     size(newdata)
     save('-ascii', '-append', filename, 'newdata');
     ax = ax+datasize;
