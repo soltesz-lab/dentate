226a9e9a4968421bff799d527361a43dc08dd972
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-04-10 13:39:19 -0700

    added IS/MOPP cell templates

---
+++ datasets/Full_Scale_Control/import_coords.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ ~/.local/bin/importcoords --header 'GC' GC.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'MC' MC.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'HC' HC.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'BC' BC.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'AAC' AAC.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'HCC' HCC.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'NGFC'  NGFC.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'IS'  IS.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'MOPP'  MOPP.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'MPP'  MPP.dat dentate_Full_Scale_Control_coords.h5
+ ~/.local/bin/importcoords --header  'LPP'  LPP.dat dentate_Full_Scale_Control_coords.h5
