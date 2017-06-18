85fcac57351a5b399416e038e0f2dbf0600af859
neurosutras <neurosutras@gmail.com>
2017-06-18 13:46:23 -0700

    061817: dev branch: Adding script to pull PP-GC synaptic weights from a log-normal distribution.

--- scripts/function_lib.py
+++ scripts/function_lib.py
@@ -198,12 +198,56 @@ def get_inhom_poisson_spike_times_by_thinning(rate, t, dt=0.02, refractory=3., g
 
 def split_array(a, size):
     start = 0
-    chunk_size = int(len(a) / size)
+    chunk_size = int(math.ceil(float(len(a)) / size))
     sub_arrays = []
-    for i in range(size):
+    for i in xrange(size):
         if i == size - 1:
             sub_arrays.append(a[start:])
         else:
             sub_arrays.append(a[start:start+chunk_size])
             start += chunk_size
     return sub_arrays
+
+
+def serial_neuroh5_get_attr(file_path, population, namespace, gid, header='Populations'):
+    """
+
+    :param file_path: str
+    :param population: str
+    :param namespace: str
+    :param gid: int
+    :param header: str
+    :return: dict
+    """
+    attr_dict = {}
+    if not os.path.isfile(file_path):
+        raise IOError('serial_neuroh5_get_attr: invalid file_path: %s' % file_path)
+    with h5py.File(file_path, 'r') as f:
+        if header not in f:
+            raise AttributeError('serial_neuroh5_get_attr: invalid header: %s' % header)
+        elif population not in f[header]:
+            raise AttributeError('serial_neuroh5_get_attr: invalid population: %s' % population)
+        elif namespace not in f[header][population]:
+            raise AttributeError('serial_neuroh5_get_attr: invalid namespace: %s' % namespace)
+        group = f[header][population][namespace]
+        gid_offset = None
+        pop_size = None
+        attr_dict[namespace] = {}
+        for attr in group:
+            if not ('gid' in group[attr] and 'ptr' in group[attr] and 'value' in group[attr]):
+                print 'serial_neuroh5_get_attr: excluding namespace: %s; attribute: %s; format not recognized' % \
+                      (namespace, attr)
+            else:
+                if gid_offset is None:
+                    gid_offset = group[attr]['gid'][0]
+                    pop_size = len(group[attr]['gid'])
+                gid_index = gid - gid_offset
+                if gid_index > pop_size:
+                    raise ValueError('serial_neuroh5_get_attr: gid: %i out of range for population: %s' %
+                                     (gid, population))
+                else:
+                    start = group[attr]['ptr'][gid_index]
+                    stop = group[attr]['ptr'][gid_index+1]
+                    attr_dict[namespace][attr] = group[attr]['value'][start:stop]
+    return attr_dict
+
