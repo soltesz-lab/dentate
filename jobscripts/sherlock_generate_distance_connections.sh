8135ef49938a332f4ce3ef559dbf849ef43d0b23
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-10-12 12:50:24 -0700

    updated arc distance script on sherlock

--- jobscripts/sherlock_generate_distance_connections.sh
+++ jobscripts/sherlock_generate_distance_connections.sh
@@ -2,8 +2,8 @@
 #
 #SBATCH -J generate_distance_connections
 #SBATCH -o ./results/generate_distance_connections.%j.o
-#SBATCH -n 36
-#SBATCH -t 6:00:00
+#SBATCH -n 48
+#SBATCH -t 8:00:00
 #SBATCH --mem-per-cpu=10G
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=END
@@ -20,10 +20,10 @@ export LD_LIBRARY_PATH=$HOME/bin/hdf5/lib:$LD_LIBRARY_PATH
 
 set -x
 
-mpirun -np 36 python ./scripts/generate_distance_connections.py \
+mpirun -np 48 python ./scripts/generate_distance_connections.py \
        --config=./config/Full_Scale_Control.yaml \
        --forest-path=$SCRATCH/dentate/DG_test_forest_syns_20170927.h5 \
-       --connectivity-path=$SCRATCH/dentate/DG_test_connections_20171011.h5 \
+       --connectivity-path=$SCRATCH/dentate/DG_test_connections_20171012.h5 \
        --connectivity-namespace=Connections \
        --coords-path=$SCRATCH/dentate/dentate_Full_Scale_Control_coords_20171005.h5 \
        --coords-namespace=Coordinates \
