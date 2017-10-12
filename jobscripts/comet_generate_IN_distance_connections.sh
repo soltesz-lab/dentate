efbced8feac9b2a2a02df6a51579b5e0d6c89f09
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-10-12 15:57:25 -0700

    updated comet jobscripts

--- jobscripts/comet_generate_IN_distance_connections.sh
+++ jobscripts/comet_generate_IN_distance_connections.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#SBATCH -J generate_distance_connections
-#SBATCH -o ./results/generate_distance_connections.%j.o
+#SBATCH -J generate_IN_distance_connections
+#SBATCH -o ./results/generate_IN_distance_connections.%j.o
 #SBATCH --nodes=24
-#SBATCH --ntasks-per-node=24
-#SBATCH -t 4:00:00
+#SBATCH --ntasks-per-node=16
+#SBATCH -t 2:00:00
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=END
 #SBATCH --mail-type=BEGIN
@@ -25,10 +25,10 @@ export LD_PRELOAD=$MPIHOME/lib/libmpi.so
 
 set -x
 
-ibrun -np 576 python ./scripts/generate_distance_connections.py \
+ibrun -np 384 python ./scripts/generate_distance_connections.py \
        --config=./config/Full_Scale_Control.yaml \
-       --forest-path=$SCRATCH/dentate/DG_test_forest_syns_20170927.h5 \
-       --connectivity-path=$SCRATCH/dentate/DG_test_connections_20171009.h5 \
+       --forest-path=$SCRATCH/dentate/DG_IN_forest_syns_20171012.h5 \
+       --connectivity-path=$SCRATCH/dentate/DG_IN_connections_20171012.h5 \
        --connectivity-namespace=Connections \
        --coords-path=$SCRATCH/dentate/dentate_Full_Scale_Control_coords_20171005.h5 \
        --coords-namespace=Coordinates \
