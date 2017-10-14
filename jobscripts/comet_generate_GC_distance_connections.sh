45b782cc9ca01e26f99eaa995654da69002ec430
Ivan Raikov <ivan.g.raikov@gmail.com>
2017-10-13 22:22:26 -0700

    adjustments to GC conn config

--- jobscripts/comet_generate_GC_distance_connections.sh
+++ jobscripts/comet_generate_GC_distance_connections.sh
@@ -2,8 +2,8 @@
 #
 #SBATCH -J generate_GC_distance_connections
 #SBATCH -o ./results/generate_GC_distance_connections.%j.o
-#SBATCH --nodes=24
-#SBATCH --ntasks-per-node=16
+#SBATCH --nodes=48
+#SBATCH --ntasks-per-node=12
 #SBATCH -t 4:00:00
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=END
@@ -22,15 +22,16 @@ export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:$PYTHONPATH
 export PYTHONPATH=$HOME/model/dentate:$PYTHONPATH
 export SCRATCH=/oasis/scratch/comet/iraikov/temp_project
 export LD_PRELOAD=$MPIHOME/lib/libmpi.so
+ulimit -c unlimited
 
 set -x
 
-ibrun -np 384 python ./scripts/generate_distance_connections.py \
+ibrun -np 576 python ./scripts/generate_distance_connections.py \
        --config=./config/Full_Scale_Control.yaml \
        --forest-path=$SCRATCH/dentate/DGC_forest_syns_20170925_compressed.h5 \
        --connectivity-path=$SCRATCH/dentate/DG_GC_connections_20171012.h5 \
        --connectivity-namespace=Connections \
        --coords-path=$SCRATCH/dentate/dentate_Full_Scale_Control_coords_20171005.h5 \
        --coords-namespace=Coordinates \
-       --io-size=32
+       --io-size=128 --cache-size=2
 
