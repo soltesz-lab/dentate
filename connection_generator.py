719a79f032ea44335a7f608e6a6bb9181504d092
Ivan Raikov <ivan.g.raikov@gmail.com>
2018-03-11 23:55:28 -0700

    added random clusters routine based on make_blobs in scikit-learn

--- connection_generator.py
+++ connection_generator.py
@@ -31,7 +31,82 @@ def random_choice_w_replacement(ranstream,n,p):
 def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum()
-    
+
+
+def make_random_clusters(centers, n_samples_per_center, n_features=2, cluster_std=1.0,
+                         center_ids=None, center_box=(-10.0, 10.0), random_state=None):
+    """Generate isotropic Gaussian blobs for clustering.
+
+    Parameters
+    ----------
+    centers : int or array of shape [n_centers, n_features]
+        The number of centers to generate, or the fixed center locations.
+    n_samples_per_center : int array
+        Number of points for each cluster.
+    n_features : int, optional (default=2)
+        The number of features for each sample.
+    cluster_std : float or sequence of floats, optional (default=1.0)
+        The standard deviation of the clusters.
+    center_ids : array of integer center ids, if None then centers will be numbered 0 .. n_centers-1
+    center_box : pair of floats (min, max), optional (default=(-10.0, 10.0))
+        The bounding box for each cluster center when centers are
+        generated at random.
+    random_state : int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
+    Returns
+    -------
+    X : array of shape [n_samples, n_features]
+        The generated samples.
+    y : array of shape [n_samples]
+        The integer labels for cluster membership of each sample.
+    Examples
+    --------
+    >>> X, y = make_blobs(n_samples={0: 3, 1: 3, 2: 3}, centers=3, n_features=2,
+    ...                   random_state=0)
+    >>> print(X.shape)
+    (10, 2)
+    >>> y
+    array([0, 0, 1, 0, 2, 2, 2, 1, 1, 0])
+    """
+    rng = np.random.RandomState(seed)
+
+    if isinstance(centers, numbers.Integral):
+        centers = np.sort(rng.uniform(center_box[0], center_box[1], \
+                                      size=(centers, n_features)), axis=0)
+    else:
+        assert(isinstance(centers, numpy.ndarray))
+        n_features = centers.shape[1]
+
+    if center_ids is None:
+        center_ids = np.arange(0, centers.shape[0])
+        
+    if isinstance(cluster_std, numbers.Real):
+        cluster_std = np.ones(len(centers)) * cluster_std
+
+    X = []
+    y = []
+
+    n_centers = centers.shape[0]
+
+    for i, (cid, n, std) in enumerate(itertools.izip(center_ids, n_samples_per_center, cluster_std)):
+        X.append(centers[i] + rng.normal(scale=std, size=(n, n_features)))
+        y += [cid] * n
+
+    X = np.concatenate(X)
+    y = np.array(y)
+
+    return X, y
+
+
+def random_clustered_shuffle(centers, n_samples_per_center, cluster_std=1.0, center_box=(-10.0, 10.0)):
+    X, y = make_random_clusters (centers, n_samples_per_center, n_features=1, \
+                                 cluster_std=cluster_std, center_box=center_box)
+    s = np.argsort(X,axis=0).ravel()
+    return y[s].ravel()
+
 
     
 class ConnectionProb(object):
