dc2a9f99040148eab738996715c5d73dee92d4a2
Ivan Raikov <ivan.g.raikov@gmail.com>
2018-05-09 14:08:13 -0700

    update to sherlock scripts

--- jobscripts/sherlock_generate_soma_coordinates.sh
+++ jobscripts/sherlock_generate_soma_coordinates.sh
@@ -3,7 +3,7 @@
 #SBATCH -J generate_soma_coordinates
 #SBATCH -o ./results/generate_soma_coordinates.%j.o
 #SBATCH -n 1
-#SBATCH -t 6:00:00
+#SBATCH -t 8:00:00
 #SBATCH --mem-per-cpu=12G
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=END
@@ -26,6 +26,6 @@ mpirun -np 1 python ./scripts/generate_soma_coordinates.py -v \
        --config=./config/Full_Scale_Control.yaml \
        --types-path=./datasets/dentate_h5types.h5 \
        --output-path=$SCRATCH/dentate/Full_Scale_Control/dentate_Full_Scale_Control_coords_20180214.h5 \
-       -i HCC -i MOPP -i NGFC -i IS -i MPP -i LPP \
+       -i LPP \
        --output-namespace='Generated Coordinates' 
 
