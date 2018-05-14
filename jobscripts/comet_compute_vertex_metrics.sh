f8b72d20689cc6a176a317447c3ace8adec20108
Ivan Raikov <ivan.g.raikov@gmail.com>
2018-05-14 16:43:08 -0700

    updated comet job scripts

--- jobscripts/comet_compute_vertex_metrics.sh
+++ jobscripts/comet_compute_vertex_metrics.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH -J compute_vertex_metrics
 #SBATCH -o ./results/compute_vertex_metrics.%j.o
-#SBATCH --nodes=64
+#SBATCH --nodes=32
 #SBATCH --ntasks-per-node=24
 #SBATCH -t 1:00:00
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
@@ -19,6 +19,7 @@ ulimit -c unlimited
 set -x
 
 input=$SCRATCH/dentate/Full_Scale_Control/DG_GC_connections_compressed_20180428.h5 
+input=$SCRATCH/dentate/Full_Scale_Control/DG_IN_connections_20180428.h5 
 
-ibrun -np 1536 $HOME/src/neuroh5/build/neurograph_vertex_metrics --indegree --outdegree -i 128 \
+ibrun -np 768 $HOME/src/neuroh5/build/neurograph_vertex_metrics --indegree --outdegree -i 128 \
     $input
