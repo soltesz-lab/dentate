3a46ec22ce4ab4d84b639f97b1df6b5c8bb21d41
Ivan Raikov <ivan.g.raikov@gmail.com>
2018-06-02 17:44:46 -0700

    removing debug prints

--- jobscripts/measure_GC_distances.sh
+++ jobscripts/measure_GC_distances.sh
@@ -1,8 +1,8 @@
 
-mpirun.mpich -np 1 python ./scripts/measure_distances.py \
+mpirun.mpich -np 8 python ./scripts/measure_distances.py \
              --config=./config/Full_Scale_Control.yaml \
              --coords-namespace=Coordinates \
              -i GC \
-              --coords-path=./datasets/DG_coords_20180530.h5 \
-              --io-size=1 -v 
+              --coords-path=./datasets/DG_coords_20180602.h5 \
+              --io-size=2 --interpolate -v 
 
