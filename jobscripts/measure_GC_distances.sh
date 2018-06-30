12076c7c6363ae7a46ade55e5ded4c9377928eed
Ivan Raikov <ivan.g.raikov@gmail.com>
2018-06-30 15:07:47 -0700

    small refinements in interpolation of distances

--- jobscripts/measure_GC_distances.sh
+++ jobscripts/measure_GC_distances.sh
@@ -3,6 +3,6 @@ mpirun.mpich -np 8 python ./scripts/measure_distances.py \
              --config=./config/Full_Scale_Control.yaml \
              --coords-namespace=Coordinates \
              -i GC --resolution 40 40 10 \
-              --coords-path=./datasets/DG_coords_20180629.h5 \
-              --io-size=2 --interpolate --ndist 1 -v 
+              --coords-path=./datasets/DG_coords_20180630.h5 \
+              --io-size=2 -v 
 
