b8d77c99a15b5ff39fde3ffe1428156b2d776b30
Darian <dhh@stanford.edu>
2018-08-01 14:57:31 -0700

    minor tweak

--- scripts/run_optimize.sh
+++ scripts/run_optimize.sh
@@ -2,15 +2,16 @@
 #!/bin/bash
 
 template='dentate_h5types.h5'
-output='MPP_grid.h5'
-output_temp='temp_MPP_grid.h5'
+output='MPP.h5'
+output_temp='temp-MPP.h5'
+threads=$1
 
 #cp $template $output
 #cp $template $output_temp
 
 echo "Running optimization routine..."
-mpiexec -n 1 python generate_DG_PP_features_reduced_h5support.py --coords-path=$template --output-path=$output -o --iterations 2 --input-path=$output_temp --verbose --lbound=1 --ubound=100
-echo "..complete... generating data output for visualization"
-mpiexec -n 1 python generate_DG_PP_data.py $output_temp
+mpirun.mpich -n $threads python generate_DG_PP_features_reduced_h5support.py --types-path=$template --output-path=$output -o --verbose --lbound=1 --ubound=100 
+#echo "..complete... generating data output for visualization"
+#mpiexec -n 1 python generate_DG_PP_data.py $output_temp
 echo "...complete..."
 
