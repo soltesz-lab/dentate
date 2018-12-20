6b8d63ea505f0383a1ece2bb28dddc819ff3bcdf
Aaron D. Milstein <neurosutras@gmail.com>
2018-12-19 17:07:03 -0800

    20181219: master:  - network: Now if the mech_file for a population includes synapse rules, connect_cells calls synapses.init_syn_mech_attrs, which follows the rules and populates the synapse attribute dict.  - network: Confirmed that 'weights' and 'g_unit' for 'AMPA' synapses in GCs are being set properly during dry_run.

--- scripts/dry_run.py
+++ scripts/dry_run.py
@@ -29,6 +29,8 @@ sys.excepthook = mpi_excepthook
 
 
 @click.command()
+@click.option("--cell-selection-path", required=False, type=click.Path(exists=True, file_okay=True, dir_okay=False),
+              help='name of file specifying subset of cells gids to be instantiated')
 @click.option("--config-file", required=True, type=str, help='model configuration file name')
 @click.option("--template-paths", type=str, default='templates',
               help='colon-separated list of paths to directories containing hoc cell templates')
@@ -54,6 +56,10 @@ sys.excepthook = mpi_excepthook
 @click.option("--stimulus-onset", type=float, default=1.0, help='starting time of stimulus (ms)')
 @click.option("--max-walltime-hours", type=float, default=1.0, help='maximum wall time (hours)')
 @click.option("--results-write-time", type=float, default=360.0, help='time to write out results at end of simulation')
+@click.option("--spike-input-path", required=False, type=click.Path(exists=True, file_okay=True, dir_okay=False),
+                  help='path to file for input spikes when cell selection is specified')
+@click.option("--spike-input-namespace", required=False, type=str,
+                  help='namespace for input spikes when cell selection is specified')
 @click.option("--dt", type=float, default=0.025, help='')
 @click.option("--ldbal", is_flag=True, help='estimate load balance based on cell complexity')
 @click.option("--lptbal", is_flag=True, help='optimize load balancing assignment with LPT algorithm')
@@ -61,10 +67,12 @@ sys.excepthook = mpi_excepthook
               help='delete from memory the synapse attributes metadata after specifying connections')
 @click.option('--verbose', '-v', is_flag=True, help='print verbose diagnostic messages while constructing the network')
 @click.option('--run-test', is_flag=True, help='whether to actually execute simulation after building network')
-def main(config_file, template_paths, hoc_lib_path, dataset_prefix, config_prefix, results_path, results_id,
-         node_rank_file, io_size, vrecord_fraction, coredat, tstop, v_init, stimulus_onset, max_walltime_hours,
-         results_write_time, dt, ldbal, lptbal, cleanup, verbose, run_test):
+def main(cell_selection_path, config_file, template_paths, hoc_lib_path, dataset_prefix, config_prefix,
+         results_path, results_id, node_rank_file, io_size, vrecord_fraction, coredat, tstop, v_init,
+         stimulus_onset, max_walltime_hours, results_write_time, spike_input_path, spike_input_namespace,
+         dt, ldbal, lptbal, cleanup, verbose, run_test):
     """
+    :param cell_selection_path: str; name of file specifying subset of cells gids to be instantiated
     :param config_file: str; model configuration file name
     :param template_paths: str; colon-separated list of paths to directories containing hoc cell templates
     :param hoc_lib_path: str; path to directory containing required hoc libraries
@@ -81,6 +89,8 @@ def main(config_file, template_paths, hoc_lib_path, dataset_prefix, config_prefi
     :param stimulus_onset: float; starting time of stimulus (ms)
     :param max_walltime_hours: float; maximum wall time (hours)
     :param results_write_time: float; time to write out results at end of simulation
+    :param spike_input_path: str; path to file for input spikes when cell selection is specified
+    :param spike_input_namespace: str;
     :param dt: float; simulation time step
     :param ldbal: bool; estimate load balance based on cell complexity
     :param lptbal: bool; calculate load balance with LPT algorithm
