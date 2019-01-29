9e2b62a3837717a865056d57aba07de7c5ca9bcc
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-01-29 07:22:55 -0800

    plot_spike_hist: use interpolation to obtain firing rates on requested bin size

--- scripts/plot_spike_hist.py
+++ scripts/plot_spike_hist.py
@@ -13,6 +13,7 @@ script_name = os.path.basename(__file__)
 @click.option("--spike-events-namespace", '-n', type=str, default='Spike Events')
 @click.option("--populations", '-i', type=str, multiple=True)
 @click.option("--spike-hist-bin", type=float, default=5.0)
+@click.option("--kernel-size", type=float, default=100.)
 @click.option("--smooth", type=int)
 @click.option("--t-variable", type=str, default='t')
 @click.option("--t-max", type=float)
@@ -22,7 +23,7 @@ script_name = os.path.basename(__file__)
 @click.option("--graph-type", type=str, default='bar')
 @click.option("--overlay", type=bool, default=False, is_flag=True)
 @click.option("--verbose", "-v", type=bool, default=False, is_flag=True)
-def main(spike_events_path, spike_events_namespace, populations, spike_hist_bin, smooth, t_variable, t_max, t_min, quantity, font_size, graph_type, overlay, verbose):
+def main(spike_events_path, spike_events_namespace, populations, spike_hist_bin, kernel_size, smooth, t_variable, t_max, t_min, quantity, font_size, graph_type, overlay, verbose):
 
     utils.config_logging(verbose)
 
@@ -38,7 +39,8 @@ def main(spike_events_path, spike_events_namespace, populations, spike_hist_bin,
         populations = ['eachPop']
         
     plot.plot_spike_histogram (spike_events_path, spike_events_namespace, include=populations, time_variable=t_variable,
-                               time_range=time_range, pop_rates=True, bin_size=spike_hist_bin, smooth=smooth, quantity=quantity, fontSize=font_size,
+                               time_range=time_range, pop_rates=True, kernel_size=kernel_size, bin_size=spike_hist_bin, smooth=smooth,
+                               quantity=quantity, fontSize=font_size,
                                overlay=overlay, graph_type=graph_type, saveFig=True)
     
 
