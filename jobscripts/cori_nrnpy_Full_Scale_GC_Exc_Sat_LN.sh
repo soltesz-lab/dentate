639f1e71d7b9382f1223304174ad02160f7c441a
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-02-05 11:48:38 -0800

    updated data creation scripts and cori job scripts

--- jobscripts/cori_nrnpy_Full_Scale_GC_Exc_Sat_LN.sh
+++ jobscripts/cori_nrnpy_Full_Scale_GC_Exc_Sat_LN.sh
@@ -4,7 +4,7 @@
 #SBATCH -o ./results/dentate_Full_Scale_GC_Exc_Sat_LN.%j.o
 #SBATCH -N 320
 #SBATCH --ntasks-per-node=32
-#SBATCH -p regular
+#SBATCH --qos=regular
 #SBATCH -t 6:30:00
 #SBATCH -L SCRATCH   # Job requires $SCRATCH file system
 #SBATCH -C haswell   # Use Haswell nodes
@@ -34,7 +34,7 @@ echo python is `which python`
 
 set -x
 
-srun -n 10240 python ./scripts/main.py \
+srun -n 10240 -c 2 python ./scripts/main.py \
  --config-file=Full_Scale_GC_Exc_Sat_LN.yaml \
  --template-paths=../dgc/Mateos-Aparicio2014:templates \
  --dataset-prefix="$SCRATCH/dentate" \
