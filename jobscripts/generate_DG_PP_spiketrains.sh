24a0154a759586a9cb493d852b2cc7f8a4e28ffa
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-05-01 22:39:31 -0700

    modifying features and spiketrains scripts to use new input feature config

--- jobscripts/generate_DG_PP_spiketrains.sh
+++ jobscripts/generate_DG_PP_spiketrains.sh
@@ -2,9 +2,10 @@
 
 mpirun.mpich -np 8  python ./scripts/generate_DG_PP_spiketrains.py \
    --config=./config/Full_Scale_Pas.yaml \
-   --features-path=./datasets/DG_PP_features_100_20190501.h5 \
-   --output-path=./datasets/DG_PP_spiketrains_100_20190501.h5 \
-   --stimulus-id=100 \
+   --features-path=./datasets/DG_PP_features_A_20190501.h5 \
+   --output-path=./datasets/DG_PP_spiketrains_A_Diag_20190501.h5 \
+   --arena-id=A \
+   --trajectory-id=Diag \
    --io-size=2 -v 
 
 
--- scripts/generate_DG_PP_features.py
+++ scripts/generate_DG_PP_features.py
@@ -34,8 +34,9 @@ def calculate_field_distribution(pi, pr):
     return probabilities 
 
 @click.command()
-@click.option("--config", required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False))
-@click.option("--stimulus-id", type=int, default=0)
+@click.option("--config", required=True, type=str)
+@click.option("--config-prefix", required=True, type=click.Path(exists=True, file_okay=False, dir_okay=True), default='config')
+@click.option("--arena-id", type=str, default='A')
 @click.option("--coords-path", required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False))
 @click.option("--output-path", required=True, type=click.Path(file_okay=True, dir_okay=False))
 @click.option("--distances-namespace", type=str, default='Arc Distances')
@@ -46,10 +47,11 @@ def calculate_field_distribution(pi, pr):
 @click.option("--write-size", type=int, default=1)
 @click.option("--verbose", '-v', is_flag=True)
 @click.option("--dry-run", is_flag=True)
-def main(config, stimulus_id, coords_path, output_path, distances_namespace, io_size, chunk_size, value_chunk_size, cache_size, write_size, verbose, dry_run):
+def main(config, config_prefix, arena_id, coords_path, output_path, distances_namespace, io_size, chunk_size, value_chunk_size, cache_size, write_size, verbose, dry_run):
     """
 
     :param config:
+    :param config_prefix:
     :param coords_path:
     :param distances_namespace:
     :param io_size:
@@ -64,7 +66,7 @@ def main(config, stimulus_id, coords_path, output_path, distances_namespace, io_
 
     config_logging(verbose)
 
-    env = Env(comm=comm, config_file=config)
+    env = Env(comm=comm, config_file=config, config_prefix=config_prefix)
     if io_size == -1:
         io_size = comm.size
     if rank == 0:
@@ -80,7 +82,7 @@ def main(config, stimulus_id, coords_path, output_path, distances_namespace, io_
     comm.barrier()
     population_ranges = read_population_ranges(coords_path, comm)[0]
 
-    input_config = env.input_config
+    input_config = env.input_config['Arena'][arena_id]
     nmodules = input_config['number modules']
     field_width_x1 = input_config['field width params']['x1']
     field_width_x2 = input_config['field width params']['x2']
@@ -88,7 +90,8 @@ def main(config, stimulus_id, coords_path, output_path, distances_namespace, io_
     resolution = input_config['resolution']
     module_pi = input_config['Perforant Path']['probability inactive']
     module_pr = input_config['Perforant Path']['probability remaining']
-    feature_type = input_config['Feature Type']
+
+    feature_dist = env.input_config['Feature Distribution']
     
     context = Struct(**dict(locals()))
 
@@ -182,7 +185,7 @@ def assign_cells_to_module(context, gid_normed_distances, p_width=2./3, displace
 ## pr - probability 
 def determine_cell_participation(context, gid_module_assignments):
 
-    feature_type_dict   = context.feature_type
+    feature_type_dict   = context.feature_dist
     feature_seed_offset = int(context.env.modelConfig['Random Seeds']['Input Features'])
     feature_type_random = np.random.RandomState(feature_seed_offset - 1)
     num_field_random    = np.random.RandomState(feature_seed_offset - 1)
@@ -202,8 +205,7 @@ def determine_cell_participation(context, gid_module_assignments):
         feature_type_values_lst = []
         feature_type_prob_lst   = []
         for t, p in viewitems(feature_type_dict[population]):
-            ti = context.env.feature_types[t]
-            feature_type_values_lst.append(ti)
+            feature_type_values_lst.append(t)
             feature_type_prob_lst.append(p)
         feature_type_values = np.asarray(feature_type_values_lst)
         feature_type_probs  = np.asarray(feature_type_prob_lst)
