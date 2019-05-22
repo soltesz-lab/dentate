511983db8ebae70e01bbd724b72a520bea1d9778
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-05-21 17:04:13 -0700

    added a script to test reading of coordinates and distances

--- jobscripts/comet_bcast_distances.sh
+++ jobscripts/comet_bcast_distances.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#SBATCH -J measure_distances
-#SBATCH -o ./results/measure_distances.%j.o
-#SBATCH --nodes=32
-#SBATCH --ntasks-per-node=20
-#SBATCH -t 4:00:00
+#SBATCH -J bcast_distances
+#SBATCH -o ./results/bcast_distances.%j.o
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=12
+#SBATCH -t 0:10:00
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=END
 #SBATCH --mail-type=BEGIN
@@ -18,17 +18,16 @@ module load mpi4py
 
 export PYTHONPATH=$HOME/.local/lib/python2.7/site-packages:/share/apps/compute/mpi4py/mvapich2_ib/lib/python2.7/site-packages:/opt/python/lib/python2.7/site-packages:$PYTHONPATH
 export PYTHONPATH=$HOME/bin/nrnpython/lib/python:$PYTHONPATH
-export PYTHONPATH=$HOME/model:$HOME/model/dentate/btmorph:$PYTHONPATH
+export PYTHONPATH=$HOME/model:$PYTHONPATH
 export SCRATCH=/oasis/scratch/comet/iraikov/temp_project
 ulimit -c unlimited
 
 set -x
 
-mpirun -np 640 python ./scripts/measure_distances.py \
-    --config=./config/Full_Scale_Pas.yaml --resolution 40 40 10 \
+mpirun -np 24 python ./scripts/bcast_coords.py \
+    --config=./config/Full_Scale_Pas.yaml \
      -i GC -i MPP -i LPP -i MC -i BC -i HC -i HCC -i NGFC -i MOPP -i IS -i AAC -i CA3c -i ConMC \
-    --coords-path=$SCRATCH/dentate/Full_Scale_Control/DG_coords_20190521.h5 \
+    --coords-path=$SCRATCH/dentate/Full_Scale_Control/DG_coords_20190518_fix.h5 \
     --coords-namespace=Coordinates \
-    --io-size=24 \
     -v
 
--- scripts/make_DG_coords_file.py
+++ scripts/make_DG_coords_file.py
@@ -7,7 +7,7 @@ DG_populations = ["AAC", "BC", "GC", "HC", "HCC", "IS", "MC", "MOPP", "NGFC", "M
 DG_IN_populations = ["AAC", "BC", "HC", "HCC", "IS", "MC", "MOPP", "NGFC"]
 DG_EXT_populations = ["MPP", "LPP", "CA3c", "ConMC"]
 
-DG_coords_file = "DG_coords_20190518.h5"
+DG_coords_file = "DG_coords_20190521.h5"
 
 DG_GC_coordinate_file  = "DGC_forest_reindex_20181222_compressed.h5"
 DG_IN_coordinate_file  = "dentate_Full_Scale_Control_coords_20180717.h5"
@@ -67,7 +67,12 @@ with h5py.File(DG_coords_file) as f:
     for p in DG_populations:
         coords_file = coordinate_files[p]
         coords_ns   = coordinate_namespaces[p]
-        grp[p]["Coordinates"] = h5py.ExternalLink(coords_file,"/Populations/%s/%s" % (p, coords_ns))
+        group_id    = f.require_group('/Populations/%s' % p)
+
+        with h5py.File(coords_file) as fs:
+            fs.copy('/Populations/%s/%s' % (p, coords_ns), group_id, name='Coordinates')
+
+                    
 
 
     
