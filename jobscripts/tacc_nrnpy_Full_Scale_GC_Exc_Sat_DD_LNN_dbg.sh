6e9de0da87a243c7d6f3a826f6124aafc6caadec
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-08-01 17:43:02 -0500

    tacc jobscripts update

--- jobscripts/tacc_nrnpy_Full_Scale_GC_Exc_Sat_DD_LNN_dbg.sh
+++ jobscripts/tacc_nrnpy_Full_Scale_GC_Exc_Sat_DD_LNN_dbg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH -J dentate_Full_Scale_GC_Exc_Sat_DD_LNN_Diag # Job name
+#SBATCH -J dentate_Full_Scale_GC_Exc_Sat_DD_LNN_dbg           # Job name
 #SBATCH -o ./results/dentate.o%j       # Name of stdout output file
 #SBATCH -e ./results/dentate.e%j       # Name of stderr error file
 #SBATCH -p skx-normal      # Queue (partition) name
 #SBATCH -N 128             # Total # of nodes 
 #SBATCH -n 6144            # Total # of mpi tasks
-#SBATCH -t 12:00:00        # Run time (hh:mm:ss)
+#SBATCH -t 02:00:00        # Run time (hh:mm:ss)
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
@@ -20,7 +20,7 @@ export NEURONROOT=$HOME/bin/nrnpython3
 export PYTHONPATH=$HOME/model:$NEURONROOT/lib/python:$PYTHONPATH
 export PATH=$NEURONROOT/x86_64/bin:$PATH
 
-results_path=$WORK/dentate/results/Full_Scale_GC_Exc_Sat_DD_LNN_Diag_$SLURM_JOB_ID
+results_path=./results/Full_Scale_GC_Exc_Sat_DD_LNN_Diag_$SLURM_JOB_ID
 export results_path
 
 cd $SLURM_SUBMIT_DIR
@@ -31,16 +31,18 @@ git ls-files | tar -zcf ${results_path}/dentate.tgz --files-from=/dev/stdin
 git --git-dir=../dgc/.git ls-files | grep Mateos-Aparicio2014 | tar -C ../dgc -zcf ${results_path}/dgc.tgz --files-from=/dev/stdin
 
 ibrun python3.7 ./scripts/main.py  \
-    --config-file=Full_Scale_GC_Exc_Sat_DD_LNN_Diag.yaml  \
+    --config-file=Full_Scale_GC_Exc_Sat_DD_LNN.yaml  \
     --arena-id=A --trajectory-id=Diag \
     --template-paths=../dgc/Mateos-Aparicio2014:templates \
     --dataset-prefix="$WORK/dentate" \
     --results-path=$results_path \
     --io-size=256 \
-    --tstop=5000 \
+    --tstop=125 \
     --v-init=-75 \
     --results-write-time=600 \
-    --max-walltime-hours=11.9 \
+    --stimulus-onset=0.0 \
+    --max-walltime-hours=2.0 \
     --vrecord-fraction=0.001 \
+    --lptbal --ldbal \
     --verbose
 
