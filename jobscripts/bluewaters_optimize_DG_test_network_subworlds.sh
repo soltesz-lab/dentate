f65754f6c7efe2e56d91e61c8f43de96beb5b4e0
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-08-07 11:46:28 -0500

    updates to bw jobscripts and in-memory spike data collection

--- jobscripts/bluewaters_optimize_DG_test_network_subworlds.sh
+++ jobscripts/bluewaters_optimize_DG_test_network_subworlds.sh
@@ -1,5 +1,5 @@
 ### set the number of nodes and the number of PEs per node
-#PBS -l nodes=72:ppn=16:xe
+#PBS -l nodes=160:ppn=8:xe
 ### which queue to use
 #PBS -q normal
 ### set the wallclock time
@@ -17,13 +17,14 @@
 
 
 module load bwpy/2.0.1
-module load craype-hugepages2M
 
 set -x
 
+export LC_ALL=en_IE.utf8
+export LANG=en_IE.utf8
 export SCRATCH=/projects/sciteam/bayj
-export NEURONROOT=$SCRATCH/nrnintel
-export PYTHONPATH=$HOME/model:$NEURONROOT/lib/python:$SCRATCH/site-packages:$PYTHONPATH
+export NEURONROOT=$SCRATCH/nrnintel3
+export PYTHONPATH=$HOME/model:$HOME/model/dentate/tests:$NEURONROOT/lib/python:$SCRATCH/site-packages:$PYTHONPATH
 export PATH=$NEURONROOT/x86_64/bin:$PATH
 export MODEL_HOME=$HOME/model
 export DG_HOME=$MODEL_HOME/dentate
@@ -35,27 +36,25 @@ cd $PBS_O_WORKDIR
 
 mkdir -p $results_path
 
-cd tests
-
-aprun -n 1152 -b -- bwpy-environ -- \
-    python2.7 -m nested.optimize  \
-     --config-file-path=$DG_HOME/config/DG_test_network_subworlds_config.yaml \
-     --output-dir=$results_path \
-     --pop_size=4 \
-     --max_iter=4 \
-     --path_length=1 \
-     --framework=pc \
-     --disp \
-     --procs-per-worker=288 \
-     --no-cleanup \
-     --verbose \
-     --template_paths=$MODEL_HOME/dgc/Mateos-Aparicio2014:$DG_HOME/templates \
-     --dataset_prefix="$SCRATCH" \
-     --config_prefix=$DG_HOME/config \
-     --results_path=$results_path \
-     --cell_selection_path=$DG_HOME/datasets/DG_slice.yaml \
-     --spike_input_path=$DG_HOME/results/Full_Scale_GC_Exc_Sat_LNN_9870802.bw/dentatenet_Full_Scale_GC_Exc_Sat_LNN_results.h5 \
-     --spike_input_namespace='Spike Events' \
-     --max-walltime-hours=3.75 \
-     --io-size=64 \
-     -v
+aprun -n 1280 -b -- bwpy-environ -- \
+    python3.6 -m nested.optimize  \
+    --config-file-path=$DG_HOME/config/DG_test_network_subworlds_config.yaml \
+    --output-dir=$results_path \
+    --pop_size=4 \
+    --max_iter=4 \
+    --path_length=1 \
+    --framework=pc \
+    --disp \
+    --verbose \
+    --procs_per_worker=320 \
+    --no_cleanup \
+    --template_paths=$MODEL_HOME/dgc/Mateos-Aparicio2014:$DG_HOME/templates \
+    --dataset_prefix="$SCRATCH" \
+    --config_prefix=$DG_HOME/config \
+    --results_path=$results_path \
+    --cell_selection_path=$DG_HOME/datasets/DG_slice_20190729.yaml \
+    --spike_input_path="$SCRATCH/Full_Scale_Control/DG_input_spike_trains_20190724_compressed.h5" \
+    --spike_input_namespace='Input Spikes' \
+    --max_walltime_hours=3.75 \
+    --io_size=24 \
+    -v
