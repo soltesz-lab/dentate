c05a761ecd244a6f066d5eae2c4c621125a91eb9
Ivan Raikov <ivan.g.raikov@gmail.com>
2019-11-17 00:37:20 -0600

    frontera config/jobsctipts update

--- jobscripts/frontera_nrnpy_Full_Scale_GC_Exc_Sat_DD_SLN_Diag.sh
+++ jobscripts/frontera_nrnpy_Full_Scale_GC_Exc_Sat_DD_SLN_Diag.sh
@@ -10,8 +10,6 @@
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
-module unload python2 
-module load python3
 module load phdf5/1.8.16
 
 set -x
