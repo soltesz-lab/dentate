30a06e8fd6badb70b2c24c277ccd9448d7201b47
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-07-09 09:12:14 -0700

    updated comet jobscripts

--- jobscripts/comet_generate_distance_connections_Test_GC_1000.sh
+++ jobscripts/comet_generate_distance_connections_Test_GC_1000.sh
@@ -10,28 +10,22 @@
 #SBATCH --mail-type=BEGIN
 #
 
-module load python
-module unload intel
-module load gnu
-module load openmpi_ib
-module load mkl
-module load hdf5
+. $HOME/comet_env.sh
 
-
-export PYTHONPATH=$HOME/.local/lib/python3.5/site-packages:/opt/sdsc/lib
-export PYTHONPATH=$HOME/bin/nrnpython3/lib/python:$PYTHONPATH
-export PYTHONPATH=$HOME/model:$PYTHONPATH
-export SCRATCH=/oasis/scratch/comet/iraikov/temp_project
 ulimit -c unlimited
 
 set -x
 
-ibrun -np 96 python3.5 ./scripts/generate_distance_connections.py \
+export SLURM_NODEFILE=`generate_pbs_nodefile`
+
+#Run the job using mpirun_rsh 
+mpirun_rsh -export-all -hostfile $SLURM_NODEFILE -np 24 \
+    python3 ./scripts/generate_distance_connections.py \
        --config-prefix=./config \
        --config=Test_GC_1000.yaml \
        --forest-path=$SCRATCH/dentate/Test_GC_1000/DG_Test_GC_1000_cells_20190625.h5 \
-       --connectivity-path=$SCRATCH/dentate/Test_GC_1000/DG_Test_GC_1000_connections_20190625.h5 \
+       --connectivity-path=$SCRATCH/dentate/Test_GC_1000/DG_Test_GC_1000_connections_20200706.h5 \
        --connectivity-namespace=Connections \
        --coords-path=$SCRATCH/dentate/Test_GC_1000/DG_coords_20190625.h5 \
        --coords-namespace=Coordinates \
-       --io-size=24 --cache-size=20 --write-size=40 --value-chunk-size=200000 --chunk-size=50000 -v --dry-run
+       --io-size=12 --cache-size=20 --write-size=10 --value-chunk-size=200000 --chunk-size=50000 -v
