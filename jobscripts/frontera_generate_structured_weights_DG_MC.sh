97a82c5d85f5b416d3fa74dbe1784af81247dbfc
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-07-11 16:30:23 -0500

    added MC optimization script

--- jobscripts/frontera_generate_structured_weights_DG_MC.sh
+++ jobscripts/frontera_generate_structured_weights_DG_MC.sh
@@ -29,9 +29,9 @@ ibrun python3 ./scripts/generate_structured_weights_as_cell_attr.py \
     -d MC -s CA3c -n GC -n MC \
     --config=./config/Full_Scale_GC_Exc_Sat_DD_SLN.yaml \
     --initial-weights-namespace='Log-Normal Weights' \
-    --initial-weights-path=$DATA_PREFIX/DG_MC_syn_weights_20200708.h5 \
+    --initial-weights-path=$DATA_PREFIX/DG_MC_syn_weights_20200708_compressed.h5 \
     --non-structured-weights-namespace='Normal Weights' \
-    --non-structured-weights-path=$DATA_PREFIX/DG_MC_syn_weights_20200708.h5 \
+    --non-structured-weights-path=$DATA_PREFIX/DG_MC_syn_weights_20200708_compressed.h5 \
     --output-weights-namespace='Structured Weights' \
     --output-weights-path=$DATA_PREFIX/DG_MC_syn_weights_S_20200708.h5 \
     --connections-path=$DATA_PREFIX/DG_MC_connections_20200708_compressed.h5 \
