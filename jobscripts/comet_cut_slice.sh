6f040bc06c2b06e588689c92956e01c3cacb9bc8
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-09-08 09:19:47 -0700

    helper scripts to read out coordinates and features of selections of cells; small updates to comet jobscripts

--- jobscripts/comet_cut_slice.sh
+++ jobscripts/comet_cut_slice.sh
@@ -2,28 +2,20 @@
 #
 #SBATCH -J dentate_cut_slice
 #SBATCH -o ./results/dentate_cut_slice.%j.o
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
-#SBATCH -p shared
-#SBATCH -t 4:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=24
+#SBATCH -p compute
+#SBATCH -t 2:00:00
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=END
 #
+. $HOME/comet_env.sh
 
-module load python
-module unload intel
-module load gnu
-module load openmpi_ib
-module load mkl
-module load hdf5
+ulimit -c unlimited
+
+export SLURM_NODEFILE=`generate_pbs_nodefile`
 
 set -x
-
-export PYTHONPATH=$HOME/.local/lib/python3.5/site-packages:/opt/sdsc/lib
-export PYTHONPATH=$HOME/bin/nrnpython3/lib/python:$PYTHONPATH
-export PYTHONPATH=$HOME/model:$PYTHONPATH
-export SCRATCH=/oasis/scratch/comet/iraikov/temp_project
-ulimit -c unlimited
 
 results_path=$SCRATCH/dentate/results/DG_slice_$SLURM_JOB_ID
 export results_path
@@ -33,7 +25,8 @@ mkdir -p $results_path
 #git ls-files | tar -zcf ${results_path}/dentate.tgz --files-from=/dev/stdin
 #git --git-dir=../dgc/.git ls-files | grep Mateos-Aparicio2014 | tar -C ../dgc -zcf ${results_path}/dgc.tgz --files-from=/dev/stdin
 
-ibrun -np 12 gdb -batch -x $HOME/gdb_script --args python3 ./scripts/cut_slice.py \
+mpirun_rsh  -export-all -hostfile $SLURM_NODEFILE  -np 48 \
+    python3 ./scripts/cut_slice.py \
     --arena-id=A --trajectory-id=Diag \
     --config=Full_Scale_GC_Exc_Sat_DD_SLN.yaml \
     --config-prefix=./config \
@@ -41,7 +34,7 @@ ibrun -np 12 gdb -batch -x $HOME/gdb_script --args python3 ./scripts/cut_slice.p
     --output-path=$results_path \
     --spike-input-path="$SCRATCH/dentate/Full_Scale_Control/DG_input_spike_trains_20190912_compressed.h5" \
     --spike-input-namespace='Input Spikes A Diag' \
-    --distance-limits -5 5 \
+    --distance-limits -25 25 \
     --write-selection \
     --verbose
 
