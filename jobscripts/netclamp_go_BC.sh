ab059b4956626bd60031aee37c94cc5fe866a08a
Darian Hadjiabadi <dhh@c191-053.frontera.tacc.utexas.edu>
2020-09-16 17:37:17 -0500

    Changed file paths to Raikov's scratch

--- jobscripts/netclamp_go_BC.sh
+++ jobscripts/netclamp_go_BC.sh
@@ -1,12 +1,12 @@
-python3  network_clamp.py go -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml --template-paths templates \
+ibrun -n 1 python3 network_clamp.py go -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml --template-paths templates \
          -p BC -g 1039000 -t 9500 \
-         --dataset-prefix /media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG \
+         --dataset-prefix /scratch1/03320/iraikov/striped/dentate \
          --config-prefix config \
-         --input-features-path '/media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG/Full_Scale_Control/DG_input_features_20200611_compressed.h5' \
+         --input-features-path /scratch1/03320/iraikov/striped/dentate/Full_Scale_Control/DG_input_features_20200910_compressed.h5 \
          --input-features-namespaces 'Place Selectivity' \
          --input-features-namespaces 'Grid Selectivity' \
          --input-features-namespaces 'Constant Selectivity' \
          --arena-id A --trajectory-id Diag \
-         --results-path results/netclamp
+         --results-path results
 
 #         -s /media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG/results/Full_Scale_GC_Exc_Sat_DD_SLN_Diag_155005/dentatenet_Full_Scale_GC_Exc_Sat_DD_SLN_results_compressed.h5 \
