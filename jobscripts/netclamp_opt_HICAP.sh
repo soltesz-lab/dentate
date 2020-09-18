2c7d9f3811699bb854c6be03a6f4e67075d2f0b5
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-09-18 09:43:44 -0500

    adapted netclamp optimization scripts for Frontera

--- jobscripts/netclamp_opt_HICAP.sh
+++ jobscripts/netclamp_opt_HICAP.sh
@@ -1,15 +1,13 @@
-mpirun.mpich -n 2 python3  network_clamp.py optimize -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml --template-paths templates \
-         -p HCC -g 1043250 -t 9500 \
-         --dataset-prefix /media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG \
-         --config-prefix config \
-         --input-features-path '/media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG/Full_Scale_Control/DG_input_features_20200611_compressed.h5' \
-         --input-features-namespaces 'Place Selectivity' \
-         --input-features-namespaces 'Grid Selectivity' \
-         --input-features-namespaces 'Constant Selectivity' \
-         --arena-id A --trajectory-id Diag \
-         --results-path results/netclamp \
-         --param-config-name "Weight all" \
-         --opt-iter 100 rate
-
-#         -s /media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG/results/Full_Scale_GC_Exc_Sat_DD_SLN_Diag_155005/dentatenet_Full_Scale_GC_Exc_Sat_DD_SLN_results_compressed.h5 \
-#         --recording-profile "Network clamp exc synaptic" \
+ibrun python3  network_clamp.py optimize -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml \
+    --template-paths templates \
+    -p HCC -g 1043250 -t 9500 \
+    --dataset-prefix /scratch1/03320/iraikov/striped/dentate \
+    --config-prefix config \
+    --input-features-path /scratch1/03320/iraikov/striped/dentate/Full_Scale_Control/DG_input_features_20200910_compressed.h5 \
+    --input-features-namespaces 'Place Selectivity' \
+    --input-features-namespaces 'Grid Selectivity' \
+    --input-features-namespaces 'Constant Selectivity' \
+    --arena-id A --trajectory-id Diag \
+    --results-path $HOME/model/dentate/results/netclamp \
+    --param-config-name "Weight all" \
+    --opt-iter 100 rate
