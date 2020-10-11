51fe50358e2774af0a21ebf0d9354e43e01a0858
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-10-11 09:48:54 -0700

    izhikevich cell test scripts

--- jobscripts/netclamp_go_BC.sh
+++ jobscripts/netclamp_go_BC.sh
@@ -1,8 +1,10 @@
-ibrun -n 1 python3 network_clamp.py go -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml --template-paths templates \
+export DATA_PREFIX=/media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG
+
+python3 network_clamp.py go -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml --template-paths templates \
          -p BC -g 1039000 -t 9500 \
-         --dataset-prefix /scratch1/03320/iraikov/striped/dentate \
+         --dataset-prefix $DATA_PREFIX \
          --config-prefix config \
-         --input-features-path /scratch1/03320/iraikov/striped/dentate/Full_Scale_Control/DG_input_features_20200910_compressed.h5 \
+         --input-features-path $DATA_PREFIX/Full_Scale_Control/DG_input_features_20200910_compressed.h5 \
          --input-features-namespaces 'Place Selectivity' \
          --input-features-namespaces 'Grid Selectivity' \
          --input-features-namespaces 'Constant Selectivity' \
--- jobscripts/netclamp_go_HICAP.sh
+++ jobscripts/netclamp_go_HICAP.sh
@@ -1,8 +1,10 @@
+export DATA_PREFIX=/media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG
+
 python3  network_clamp.py go -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml --template-paths templates \
          -p HCC -g 1043250 -t 9500 \
-         --dataset-prefix /media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG \
+         --dataset-prefix $DATA_PREFIX \
          --config-prefix config \
-         --input-features-path '/media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG/Full_Scale_Control/DG_input_features_20200611_compressed.h5' \
+         --input-features-path "$DATA_PREFIX/Full_Scale_Control/DG_input_features_20200611_compressed.h5" \
          --input-features-namespaces 'Place Selectivity' \
          --input-features-namespaces 'Grid Selectivity' \
          --input-features-namespaces 'Constant Selectivity' \
