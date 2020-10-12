613bd90e98ae9929f3bb70ddb11ad1972ca43181
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-10-12 15:00:46 -0500

    frontera jobscripts update

--- jobscripts/frontera_submit_netclamp_opt_pf_rate_GC.sh
+++ jobscripts/frontera_submit_netclamp_opt_pf_rate_GC.sh
@@ -4,6 +4,6 @@
 export job_name="netclamp_opt_pf_rate_${1}"
 export output_name="./results/netclamp_opt_pf_rate_${1}.%j.o"
 
-echo sbatch -J $job_name -o $output_name ./jobscripts/frontera_netclamp_opt_pf_rate.sh $1 $2
-sbatch -J $job_name -o $output_name ./jobscripts/frontera_netclamp_opt_pf_rate.sh $1 $2
+echo sbatch -J $job_name -o $output_name ./jobscripts/frontera_netclamp_opt_pf_rate_GC.sh $1 $2
+sbatch -J $job_name -o $output_name ./jobscripts/frontera_netclamp_opt_pf_rate_GC.sh $1 $2
 
--- scripts/make_DG_data_files.py
+++ scripts/make_DG_data_files.py
@@ -8,17 +8,17 @@ DG_populations = ["AAC", "BC", "GC", "HC", "HCC", "IS", "MC", "MOPP", "NGFC", "M
 DG_IN_populations = ["AAC", "BC", "HC", "HCC", "IS", "MC", "MOPP", "NGFC"]
 DG_EXT_populations = ["MPP", "LPP", "CA3c"]
 
-DG_cells_file = "DG_Cells_Full_Scale_20200311.h5"
-DG_connections_file = "DG_Connections_Full_Scale_20200311.h5"
+DG_cells_file = "DG_Cells_Full_Scale_20201008.h5"
+DG_connections_file = "DG_Connections_Full_Scale_20201008.h5"
 
 DG_GC_coordinate_file  = "DG_coords_20190717_compressed.h5"
 DG_IN_coordinate_file  = "DG_coords_20190717_compressed.h5"
 DG_EXT_coordinate_file = "DG_coords_20190717_compressed.h5"
 
-DG_GC_forest_file = "DGC_forest_normalized_20200311_compressed.h5"
+DG_GC_forest_file = "DGC_forest_normalized_20200628_compressed.h5"
 DG_IN_forest_file = "DG_IN_forest_20200112_compressed.h5"
 
-DG_GC_forest_syns_file = "DGC_forest_syns_20190717_compressed.h5"
+DG_GC_forest_syns_file = "DGC_forest_syns_20190628_compressed.h5"
 DG_IN_forest_syns_file = "DG_IN_forest_syns_20200112_compressed.h5"
 
 DG_GC_syn_weights_SLN_file = "DG_GC_syn_weights_SLN_20191128_compressed.h5"
@@ -50,9 +50,9 @@ DG_vecstim_file_dict = {
 
 vecstim_dict = {'Input Spikes %s' % stim_id : stim_file for stim_id, stim_file in viewitems(DG_vecstim_file_dict)}
 
-DG_remap_vecstim_file_dict = { 
-    'A Diag': "DG_remap_spike_trains_20191113_compressed.h5",
-}
+#DG_remap_vecstim_file_dict = { 
+#    'A Diag': "DG_remap_spike_trains_20191113_compressed.h5",
+#}
 
 
 coordinate_files = {
