5b685f4d191bd4469eaa2e38f75777650c400ce5
Prannath Moolchand <prannath_moolchand@alumni.brown.edu>
2020-10-23 00:20:51 -0500

    20201022 - uodated Izhi params

--- jobscripts/netclamp_opt_HIPP.sh
+++ jobscripts/netclamp_opt_HIPP.sh
@@ -1,9 +1,9 @@
-export LD_PRELOAD=/opt/intel/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_core.so:/opt/intel/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_sequential.so
+#export LD_PRELOAD=/opt/intel/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_core.so:/opt/intel/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_sequential.so
 export FI_MLX_ENABLE_SPAWN=yes
 
 ibrun -n 8 python3  network_clamp.py optimize -c 20201022_Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml \
     --template-paths templates \
-    -p NGFC -g 1044650 -t 9500 \
+    -p HC -g 1030000 -t 9500 \
     --dataset-prefix /scratch1/03320/iraikov/striped/dentate \
     --config-prefix config \
     --input-features-path /scratch1/03320/iraikov/striped/dentate/Full_Scale_Control/DG_input_features_20200910_compressed.h5 \
