efe529c03d9acb9bd640ed76682d2f2b040e64ab
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-10-29 12:36:05 -0500

    calibration of MC excitatory conductances

--- jobscripts/netclamp_opt_MC.sh
+++ jobscripts/netclamp_opt_MC.sh
@@ -1,9 +1,9 @@
-#export LD_PRELOAD=/opt/intel/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_core.so:/opt/intel/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_sequential.so
+export LD_PRELOAD=$MKLROOT/lib/intel64_lin/libmkl_core.so:$MKLROOT/lib/intel64_lin/libmkl_sequential.so
 export FI_MLX_ENABLE_SPAWN=yes
 
-ibrun -n 8 python3  network_clamp.py optimize -c 20201022_Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml \
-    --template-paths templates \
-    -p MC -g 1000000 -t 9500 \
+ibrun -n 8 python3  network_clamp.py optimize -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh_extent.yaml \
+    --template-paths templates --dt 0.01 \
+    -p MC -g 1015624 -t 9500 \
     --dataset-prefix /scratch1/03320/iraikov/striped/dentate \
     --config-prefix config \
     --input-features-path /scratch1/03320/iraikov/striped/dentate/Full_Scale_Control/DG_input_features_20200910_compressed.h5 \
@@ -11,6 +11,6 @@ ibrun -n 8 python3  network_clamp.py optimize -c 20201022_Network_Clamp_GC_Exc_S
     --input-features-namespaces 'Grid Selectivity' \
     --input-features-namespaces 'Constant Selectivity' \
     --arena-id A --trajectory-id Diag \
-    --results-path $SCRATCH/model/dentate/results/netclamp \
+    --results-path results/netclamp \
     --param-config-name "Weight inh" \
     --opt-iter 400 rate
