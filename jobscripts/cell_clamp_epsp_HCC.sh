aee783a438beae9dc1a6173a0b311631ee9445bf
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-11-06 04:19:02 -0800

    netclamp HCC merge

--- jobscripts/cell_clamp_epsp_HCC.sh
+++ jobscripts/cell_clamp_epsp_HCC.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 export LD_PRELOAD=$MKLROOT/lib/intel64_lin/libmkl_core.so:$MKLROOT/lib/intel64_lin/libmkl_sequential.so
-export DATA_PREFIX=/scratch1/03320/iraikov/striped/dentate 
+export DATA_PREFIX=/media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG
 
-ibrun -n 1 python3 ./cell_clamp.py \
-        -g 1043250\
+mpirun -n 1 python3 ./cell_clamp.py \
+        -g 1043250 \
         --population HCC \
-        --config-prefix $HOME/model/dentate/config \
-        --config-file Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml \
+        --config-prefix config \
+        --config-file 20201022_Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml \
         --dataset-prefix $DATA_PREFIX \
         --template-paths=$HOME/model/DGC/Mateos-Aparicio2014:templates \
         --results-path=results/cell_clamp \
-        --presyn-name MPP \
+        --presyn-name CA3c \
         --syn-mech-name AMPA \
         --swc-type apical \
         --erev 0 \
         --v-init -60 \
         --syn-weight 1 \
-        --syn-weight 20 \
         -m psp
 
 
--- jobscripts/netclamp_go_HICAP.sh
+++ jobscripts/netclamp_go_HICAP.sh
@@ -1,16 +1,16 @@
 export DATA_PREFIX=/media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG
 
-python3  network_clamp.py go -c Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml --template-paths templates \
-         -p HCC -g 1043250 -t 9500 \
+python3  network_clamp.py go -c 20201022_Network_Clamp_GC_Exc_Sat_SLN_IN_Izh.yaml \
+         --template-paths templates \
+         -p HCC -g 1043250 -t 9500 --dt 0.001 \
          --dataset-prefix $DATA_PREFIX \
          --config-prefix config \
-         --input-features-path "$DATA_PREFIX/Full_Scale_Control/DG_input_features_20200611_compressed.h5" \
+         --input-features-path $DATA_PREFIX/Full_Scale_Control/DG_input_features_20200910_compressed.h5 \
          --input-features-namespaces 'Place Selectivity' \
          --input-features-namespaces 'Grid Selectivity' \
          --input-features-namespaces 'Constant Selectivity' \
          --arena-id A --trajectory-id Diag \
          --results-path results/netclamp \
+
          --params-path config/20201105_Izhi_compiled.yaml
 
-#         -s /media/igr/d865f900-7fcd-45c7-a7a7-bd2a7391bc40/Data/DG/results/Full_Scale_GC_Exc_Sat_DD_SLN_Diag_155005/dentatenet_Full_Scale_GC_Exc_Sat_DD_SLN_results_compressed.h5 \
-#         --recording-profile "Network clamp exc synaptic" \
