101fa586cfff540647cf5df91ff221aa2544a5c9
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-12-16 08:26:34 -0800

    updates to optimize network script on comet

--- jobscripts/comet_optimize_DG_network.sh
+++ jobscripts/comet_optimize_DG_network.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=64
 #SBATCH --ntasks-per-node=24
 #SBATCH -p compute
-#SBATCH -t 7:00:00
+#SBATCH -t 14:00:00
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=END
 #
@@ -22,11 +22,10 @@ export PYTHONPATH=$HOME/bin/nrnpython3/lib/python:$PYTHONPATH
 export PYTHONPATH=$HOME/model:$PYTHONPATH
 export SCRATCH=/oasis/scratch/comet/iraikov/temp_project
 
-#results_path=$SCRATCH/dentate/results/optimize_DG_network_$SLURM_JOB_ID
-results_path=/oasis/scratch/comet/iraikov/temp_project/dentate/results/optimize_DG_network_37370866
+results_path=$SCRATCH/dentate/results/optimize_DG_network_$SLURM_JOB_ID
 export results_path
 
-#mkdir -p $results_path
+mkdir -p $results_path
 
 ibrun -n 9 python3 optimize_network.py \
     --config-path=$DG_HOME/config/DG_optimize_network.yaml \
@@ -35,7 +34,7 @@ ibrun -n 9 python3 optimize_network.py \
     --target-rate-map-namespace="Place Selectivity" \
     --verbose \
     --nprocs-per-worker=190 \
-    --n-iter=1 \
+    --n-iter=10 \
     --no_cleanup \
     --arena_id=A --trajectory_id=Diag \
     --template_paths=$MODEL_HOME/dgc/Mateos-Aparicio2014:$DG_HOME/templates \
@@ -45,8 +44,8 @@ ibrun -n 9 python3 optimize_network.py \
     --spike_input_path="$SCRATCH/dentate/Full_Scale_Control/DG_input_spike_trains_20200910_compressed.h5" \
     --spike_input_namespace='Input Spikes A Diag' \
     --spike_input_attr='Spike Train' \
-    --max_walltime_hours=7 \
-    --io_size=24 \
+    --max_walltime_hours=14 \
+    --io_size=12 \
     --microcircuit_inputs \
     --verbose
 
