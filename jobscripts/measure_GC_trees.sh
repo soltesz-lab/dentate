04f6fbf508678124a998ca4be31c2eeca48302bc
Ivan Raikov <ivan.g.raikov@gmail.com>
2021-02-19 09:33:02 -0600

    additions to measure_trees to include dendritic diameter as function of distance to soma

--- jobscripts/measure_GC_trees.sh
+++ jobscripts/measure_GC_trees.sh
@@ -1,14 +1,12 @@
 
-module load intel/18.0.5
 module load python3
 module load phdf5
 
 set -x
 
-export LD_PRELOAD=/opt/intel/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_core.so:/opt/intel/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_sequential.so
-export NEURONROOT=$HOME/bin/nrnpython3
-export PYTHONPATH=$HOME/model:$NEURONROOT/lib/python:$SCRATCH/site-packages:$PYTHONPATH
-export PATH=$NEURONROOT/x86_64/bin:$PATH
+export NEURONROOT=$SCRATCH/bin/nrnpython3_intel19
+export PYTHONPATH=$HOME/model:$NEURONROOT/lib/python:$SCRATCH/site-packages/intel19:$PYTHONPATH
+export PATH=$NEURONROOT/bin:$PATH
 export MODEL_HOME=$HOME/model
 export DG_HOME=$MODEL_HOME/dentate
 
@@ -16,6 +14,7 @@ ibrun -np 4 python3 ./scripts/measure_trees.py \
     --config=./config/Full_Scale_Basis.yaml \
     --template-path=$HOME/model/dgc/Mateos-Aparicio2014:templates \
     -i GC \
-    --forest-path=$SCRATCH/dentate/Slice/dentatenet_Full_Scale_GC_Exc_Sat_DD_SLN_extent_arena_margin_20200901_compressed.h5 \
+    --forest-path=$SCRATCH/striped/dentate/Slice/dentatenet_Full_Scale_GC_Exc_Sat_SLN_proximal_pf_20210114.h5  \
+    --output-path=$SCRATCH/dentate/results/measure_trees.h5 \
     --io-size=1 -v
 
