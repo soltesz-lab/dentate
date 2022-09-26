2f5828f86239d36ec010f5b71ce3b902175e012b
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-09-26 05:43:17 -0700

    added a script to create a features neuroh5 file from a csv

--- scripts/write_features.py
+++ scripts/write_features.py
@@ -1,6 +1,7 @@
-import sys
+import os, sys
 from mpi4py import MPI
 from neuroh5.io import read_population_ranges, read_population_names, append_cell_attributes
+import h5py
 import numpy as np
 import pandas as pd
 import click, logging
@@ -26,18 +27,17 @@ logger = logging.getLogger(script_name)
 @click.option("--features-path", required=True, type=click.Path(exists=False, file_okay=True, dir_okay=False))
 @click.option("--features-namespace", type=str, required=True)
 @click.option("--input-path", required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False))
-def main(population, features_path, features_namespace, input_path):
+@click.option("--h5types-path", required=True, type=click.Path(exists=True, file_okay=True, dir_okay=False))
+def main(population, features_path, features_namespace, input_path, h5types_path):
 
     comm = MPI.COMM_WORLD
     rank = comm.rank
     size = comm.size
 
-    population_ranges = read_population_ranges(features_path)[0]
-
     df = None
     if rank == 0:
         df = pd.read_csv(input_path)
-        df = df.reset_index()
+        df = df.set_index('gid')
 
     df = comm.bcast(df, root=0)
     
@@ -45,18 +45,18 @@ def main(population, features_path, features_namespace, input_path):
               'Num Fields', 'Peak Rate', 'Selectivity Type', 'Module ID']
     
     features_dict = {}
-    for cell_gid, row in df.iterrows():
+    for i, (cell_gid, row) in enumerate(df.iterrows()):
 
-        if cell_gid % size != 0:
+        if i % size != rank:
             continue
-        
+
         cell_field_width = np.asarray([row['Field Width']], dtype=np.float32)
         cell_xoffset = np.asarray([row['X Offset']], dtype=np.float32)
-        cell_yoffset = np.asarray([features_dict['Y Offset']], dtype=np.float32)
-        cell_num_fields = np.asarray([features_dict['Num Fields']], dtype=np.uint8)
-        cell_module_id = np.asarray([features_dict['Module ID']], dtype=np.int8)
-        cell_peak_rate = np.asarray([features_dict['Peak Rate']], dtype=np.float32)
-        cell_selectivity_type = np.asarray([features_dict['Selectivity Type']], dtype=np.uint8)
+        cell_yoffset = np.asarray([row['Y Offset']], dtype=np.float32)
+        cell_num_fields = np.asarray([row['Num Fields']], dtype=np.uint8)
+        cell_module_id = np.asarray([row['Module ID']], dtype=np.int8)
+        cell_peak_rate = np.asarray([row['Peak Rate']], dtype=np.float32)
+        cell_selectivity_type = np.asarray([row['Selectivity Type']], dtype=np.uint8)
         
         features_dict[cell_gid] = {'Field Width': cell_field_width,
                                    'X Offset': cell_xoffset,
@@ -67,6 +67,15 @@ def main(population, features_path, features_namespace, input_path):
                                    'Selectivity Type': cell_selectivity_type,
                                    }
 
+
+    if rank == 0:
+        if not os.path.isfile(features_path):
+            input_file = h5py.File(h5types_path, 'r')
+            output_file = h5py.File(features_path, 'w')
+            input_file.copy('/H5Types', output_file)
+            input_file.close()
+            output_file.close()
+    comm.barrier()
     
     append_cell_attributes(features_path, population, features_dict,
                            namespace=features_namespace, comm=comm)
