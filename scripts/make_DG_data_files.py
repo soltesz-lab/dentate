1ce5132afb22cb3543d2d171c038b4674bce195c
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-10-21 07:54:29 -0500

    harmonizing projection configration across different connections files

--- scripts/make_DG_data_files.py
+++ scripts/make_DG_data_files.py
@@ -13,8 +13,8 @@ DG_populations = ["AAC", "BC", "GC", "HC", "HCC", "IS", "MC", "MOPP", "NGFC", "M
 DG_IN_populations = ["AAC", "BC", "HC", "HCC", "IS", "MC", "MOPP", "NGFC"]
 DG_EXT_populations = ["MPP", "LPP", "CA3c", "ConMC"]
 
-DG_cells_file = "DG_Cells_Full_Scale_20220201.h5"
-DG_connections_file = "DG_Connections_Full_Scale_20220201.h5"
+DG_cells_file = "DG_Cells_Full_Scale_20221020.h5"
+DG_connections_file = "DG_Connections_Full_Scale_20221020.h5"
 
 DG_GC_coordinate_file  = "DG_coords_20190717_compressed.h5"
 DG_IN_coordinate_file  = "DG_coords_20190717_compressed.h5"
