9d804e993e636aef52c539da021c373ba12078f0
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-12-30 22:25:29 -0600

    bug fixes in clustered synapses distribution script

--- scripts/distribute_clustered_synapse_locs.py
+++ scripts/distribute_clustered_synapse_locs.py
@@ -9,7 +9,7 @@ from dentate.env import Env
 from dentate.neuron_utils import configure_hoc_env
 from dentate.cells import load_cell_template
 from dentate import minmax_kmeans
-from neuroh5.io import scatter_read_trees, scatter_read_cell_attributes, append_cell_attributes, read_population_ranges
+from neuroh5.io import NeuroH5CellAttrGen, scatter_read_trees, scatter_read_cell_attributes, append_cell_attributes, read_population_ranges
 import h5py
 
 sys_excepthook = sys.excepthook
@@ -21,7 +21,13 @@ def mpi_excepthook(type, value, traceback):
         MPI.COMM_WORLD.Abort(1)
 sys.excepthook = mpi_excepthook
 
+def merge_dict(d1, d2, datatype):
+    dd = {}
+    for d in (d1, d2):
+        dd.update(d)
+    return dd
 
+mpi_op_merge_dict = MPI.Op.Create(merge_dict, commute=True)
             
         
 @click.command()
@@ -37,11 +43,12 @@ sys.excepthook = mpi_excepthook
 @click.option("--chunk-size", type=int, default=1000)
 @click.option("--value-chunk-size", type=int, default=1000)
 @click.option("--write-size", type=int, default=1)
+@click.option("--attr-gen-cache-size", type=int, default=10)
 @click.option("--verbose", "-v", is_flag=True)
 @click.option("--dry-run", is_flag=True)
 @click.option("--debug", is_flag=True)
 def main(config, config_prefix, template_path, output_path, forest_path, structured_weights_path, populations, arena_id, io_size, chunk_size, value_chunk_size,
-         write_size, verbose, dry_run, debug):
+         write_size, attr_gen_cache_size, verbose, dry_run, debug):
     """
 
     :param config:
@@ -109,6 +116,8 @@ def main(config, config_prefix, template_path, output_path, forest_path, structu
 
         cell_dicts = {}
 
+        node_allocation = set([])
+
         trees, _ = scatter_read_trees(forest_path, population, topology=True, comm=env.comm, io_size=io_size)
         for this_gid, this_morph_dict in trees:
             morph_dict = this_morph_dict
@@ -128,37 +137,78 @@ def main(config, config_prefix, template_path, output_path, forest_path, structu
                                      'morph_dict': morph_dict,
                                      'sec_dict': cell_sec_dict, 
                                      'secidx_dict': cell_secidx_dict}
+            node_allocation.add(this_gid)
+
+        env.node_allocation = node_allocation
 
         env.comm.barrier()
+        if rank == 0:
+            logger.info("done reading trees")
 
-        syn_attrs = None
-        syn_attrs_dict = scatter_read_cell_attributes(forest_path, population,
-                                                      namespaces=["Synapse Attributes"],
-                                                      comm=env.comm, io_size=io_size)
-        for this_gid, this_syn_attrs in syn_attrs_dict["Synapse Attributes"]:
-            cell_dicts[this_gid]['syn_attrs'] = this_syn_attrs
+
+        syn_ids_ind = None
+        syn_locs_ind = None
+        syn_layers_ind = None
+        syn_types_ind = None
+        swc_types_ind = None
+        syn_secs_ind = None
+        syn_locs_ind = None
+
+        synapses_attr_gen = NeuroH5CellAttrGen(forest_path, population,
+                                               namespace="Synapse Attributes",
+                                               mask=set(['syn_ids', 'syn_locs', 'syn_secs', 
+                                                         'syn_layers', 'syn_types', 'swc_types']), 
+                                               return_type='tuple',
+                                               comm=env.comm, io_size=io_size,
+                                               cache_size=attr_gen_cache_size,
+                                               node_allocation=env.node_allocation)
+
+        for this_gid, this_syn_attrs in synapses_attr_gen:
+
+            if this_gid is not None:
+                (attr_tuple, attr_tuple_index) = this_syn_attrs
+                if syn_ids_ind is None:
+                    syn_ids_ind = attr_tuple_index.get('syn_ids', None)
+                    syn_locs_ind = attr_tuple_index.get('syn_locs', None)
+                    syn_layers_ind = attr_tuple_index.get('syn_layers', None)
+                    syn_types_ind = attr_tuple_index.get('syn_types', None)
+                    swc_types_ind = attr_tuple_index.get('swc_types', None)
+                    syn_secs_ind = attr_tuple_index.get('syn_secs', None)
+                    syn_locs_ind = attr_tuple_index.get('syn_locs', None)
+                    
+                cell_dicts[this_gid]['syn_attrs'] = attr_tuple
 
         env.comm.barrier()
+        if rank == 0:
+            logger.info("done reading synapse attributes")
 
         gids = []
         input_rank_attr_dict = scatter_read_cell_attributes(structured_weights_path, population,
                                                             namespaces=[input_rank_namespace],
-                                                            comm=env.comm, io_size=io_size)
-        
-        for this_gid, this_syn_rank_attrs in input_rank_attr_dict[input_rank_namespace]:
+                                                            return_type='tuple',
+                                                            comm=env.comm, io_size=io_size,
+                                                            node_allocation=env.node_allocation)
+        (syn_rank_attr_iter, syn_rank_attr_tuple_index) = input_rank_attr_dict[input_rank_namespace]
+        syn_id_rank_ind = syn_rank_attr_tuple_index.get('syn_id', None)
+        syn_source_rank_ind = syn_rank_attr_tuple_index.get('source', None)
+        syn_rank_ind = syn_rank_attr_tuple_index.get('rank', None)
+
+        for this_gid, this_syn_rank_attr_tuple in syn_rank_attr_iter:
 
             syn_attrs = cell_dicts[this_gid]['syn_attrs']
             syn_source_rank_dict = { syn_id: (rank, source) for syn_id, source, rank in
-                                     zip(this_syn_rank_attrs['syn_id'], this_syn_rank_attrs['source'], this_syn_rank_attrs['rank']) }
+                                     zip(this_syn_rank_attr_tuple[syn_id_rank_ind], 
+                                         this_syn_rank_attr_tuple[syn_source_rank_ind], 
+                                         this_syn_rank_attr_tuple[syn_rank_ind]) }
             syn_attrs_dict = { syn_id: (syn_source_rank_dict[syn_id][0], 
-                                syn_source_rank_dict[syn_id][1], 
+                                        syn_source_rank_dict[syn_id][1], 
                                         syn_type, swc_type, layer, sec) 
                                for syn_id, syn_type, layer, swc_type, sec in 
-                               zip(syn_attrs['syn_ids'], 
-                                   syn_attrs['syn_types'],
-                                   syn_attrs['syn_layers'],
-                                   syn_attrs['swc_types'],
-                                   syn_attrs['syn_secs']) if syn_id in syn_source_rank_dict }    
+                               zip(syn_attrs[syn_ids_ind], 
+                                   syn_attrs[syn_types_ind],
+                                   syn_attrs[syn_layers_ind],
+                                   syn_attrs[swc_types_ind],
+                                   syn_attrs[syn_secs_ind]) if syn_id in syn_source_rank_dict }    
             cell_dicts[this_gid]['syn_attrs'] = syn_attrs_dict
             gids.append(this_gid)
 
@@ -249,8 +299,10 @@ def main(config, config_prefix, template_path, output_path, forest_path, structu
             
             if (not dry_run) and (write_size > 0) and (i % write_size == 0):
                 append_cell_attributes(output_path, population, gid_synapse_dict,
-                                       namespace='Synapse Attributes', comm=env.comm, io_size=io_size, 
-                                       chunk_size=chunk_size, value_chunk_size=value_chunk_size)
+                                       namespace=f'Synapse Clusters {arena_id}', 
+                                       comm=env.comm, io_size=io_size, 
+                                       chunk_size=chunk_size, 
+                                       value_chunk_size=value_chunk_size)
                 gid_synapse_dict = {}
 
             if debug and i == 2:
@@ -260,8 +312,10 @@ def main(config, config_prefix, template_path, output_path, forest_path, structu
         env.comm.barrier()
         if not dry_run:
             append_cell_attributes(output_path, population, gid_synapse_dict,
-                                   namespace='Synapse Attributes', comm=env.comm, io_size=io_size, 
-                                   chunk_size=chunk_size, value_chunk_size=value_chunk_size)
+                                   namespace=f'Synapse Clusters {arena_id}', 
+                                   comm=env.comm, io_size=io_size, 
+                                   chunk_size=chunk_size, 
+                                   value_chunk_size=value_chunk_size)
 
         global_count = env.comm.reduce(gid_count, op=MPI.SUM, root=0)
         if rank == 0:
