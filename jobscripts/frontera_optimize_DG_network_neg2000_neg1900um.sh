e9615eb978cbd0601a734c0aa29bcf1fe5e9fcfd
Ivan Raikov <ivan.g.raikov@gmail.com>
2023-03-08 18:10:18 -0600

    using SynExp3NMDA2fd model in slice optimization

--- jobscripts/frontera_optimize_DG_network_neg2000_neg1900um.sh
+++ jobscripts/frontera_optimize_DG_network_neg2000_neg1900um.sh
@@ -3,10 +3,10 @@
 #SBATCH -J optimize_DG_network_neg2000_neg1900um # Job name
 #SBATCH -o ./results/optimize_DG_network_neg2000_neg1900um.o%j       # Name of stdout output file
 #SBATCH -e ./results/optimize_DG_network_neg2000_neg1900um.e%j       # Name of stderr error file
-#SBATCH -p normal      # Queue (partition) name
-#SBATCH -N 512             # Total # of nodes 
+#SBATCH -p large      # Queue (partition) name
+#SBATCH -N 1024             # Total # of nodes 
 #SBATCH --ntasks-per-node=56 # # of mpi tasks per node
-#SBATCH -t 24:00:00        # Run time (hh:mm:ss)
+#SBATCH -t 36:00:00        # Run time (hh:mm:ss)
 #SBATCH --mail-user=ivan.g.raikov@gmail.com
 #SBATCH --mail-type=all    # Send email at begin and end of job
 
@@ -27,7 +27,6 @@ export CDTools=/home1/apps/CDTools/1.2
 export PATH=${CDTools}/bin:$NEURONROOT/bin:$PATH
 export MODEL_HOME=$HOME/model
 export DG_HOME=$MODEL_HOME/dentate
-export FI_MLX_ENABLE_SPAWN=1
 
 export I_MPI_ADJUST_SCATTER=2
 export I_MPI_ADJUST_SCATTERV=2
@@ -52,12 +51,13 @@ ibrun -np 28545 \
     python3 optimize_network.py \
     --config-path=$DG_HOME/config/DG_optimize_network_neg2000_neg1900um.yaml \
     --optimize-file-dir=$results_path \
+    --optimize-file-name=dmosopt.optimize_network_20230308_1600.h5 \
     --target-features-path="$DATA_PREFIX/Full_Scale_Control/DG_input_features_20220216.h5" \
     --target-features-namespace="Place Selectivity" \
     --verbose \
     --nprocs-per-worker=223 \
     --n-epochs=1 \
-    --n-initial=100 --initial-method="slh" --num-generations=400 --population-size=127 --resample-fraction 1.0 \
+    --n-initial=50 --initial-method="slh" --num-generations=400 --population-size=127 --resample-fraction 1.0 \
     --no_cleanup \
     --arena_id=A --trajectory_id=Diag \
     --template_paths=$DG_HOME/templates \
