155660765e385c91f7d34a25f0d9ac25c8a2c18b
Ivan Raikov <ivan.g.raikov@gmail.com>
2023-06-13 21:58:15 -0500

    fixes related to phenotype infrastructure

--- network.py
+++ network.py
@@ -196,7 +196,8 @@ def connect_cells(env):
 
             phenotype_dict = {}
 
-            phenotype_namespaces = ["Phenotype ID"]
+            phenotype_namespace = "Phenotype ID"
+            phenotype_namespaces = [phenotype_namespace]
             phenotype_attr_mask = set(['phenotype_id'])
 
             if env.node_allocation is None:
@@ -366,9 +367,9 @@ def connect_cells(env):
                     if phenotype_dict is not None:
                         phenotype_id = phenotype_dict[gid]
 
-                        phenotype_syn_param_tuples = phenotype_config[[postsyn_name]phenotype_id]
+                        phenotype_syn_param_tuples = phenotype_config[postsyn_name][phenotype_id]
                         
-                        for param_tuple, param_value in param_tuples:
+                        for param_tuple, param_value in phenotype_syn_param_tuples:
                             assert postsyn_name == param_tuple.population
                             
                             source = param_tuple.source
@@ -973,7 +974,7 @@ def make_cells(env):
                         if rank == 0:
                             logger.info(f"*** Creating {pop_name} gid {gid}")
                             
-                        cell = make_cell_from_tree(env, gid, pop_name, mech_dict, tree_dict, phenotype_config, 
+                        cell = make_cell_from_tree(env, gid, pop_name, mech_dict, tree_dict, 
                                                    mech_file_path, first_gid, is_izhikevich, is_PR, is_SC)
                         num_cells += 1
             else:
